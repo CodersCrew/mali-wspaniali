4b5c1b3ddabb26ecacf62d0901f1bb520a4f1eaf
KamilaCoder <kamila.spodymek@gmail.com>
2020-02-18 18:49:42 +0100

    fix linter warinings - they are treated as errors with process.env.CI = true, create action for build and deploy

--- src/internationalization/i18n.ts
+++ src/internationalization/i18n.ts
@@ -1,9 +1,9 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import translationDe from "./locales/de/translation.json";
-import translationEn from "./locales/en/translation.json";
-import translationPl from "./locales/pl/translation.json";
+import translationDe from './locales/de/translation.json';
+import translationEn from './locales/en/translation.json';
+import translationPl from './locales/pl/translation.json';
 
 i18n
     .use(LanguageDetector)
