6b73d28ae8c5cc2df0213484aff26b4e4d9aafda
Paweł Tuliński <pawel.tulinski@gmail.com>
2020-02-20 16:04:02 +0100

    Connect to Firestore

--- src/pages/AdminResultsPage/AdminResultsPage.tsx
+++ src/pages/AdminResultsPage/AdminResultsPage.tsx
@@ -1,92 +1,154 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Typography, Table, TableBody, TableHead, TableRow, TableCell, TableFooter, TablePagination } from '@material-ui/core/';
+import { Container, Typography, Table, TableBody, TableHead, TableRow, TableCell, TableFooter, TablePagination, IconButton } from '@material-ui/core/';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import i18next from "i18next"; // ADD LATER
 import { firebase } from '../../firebase/Firebase';
 import exampleData from './exampleData';
 
 const AdminResultsPage = () => {
-  const [data, setData] = useState();
+  const [data, setData] = useState<Array<firebase.firestore.DocumentData>>([]);
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
   const [lastVisible, setLastVisible] = useState();
-  const [itemsCount, setItemsCount] = useState();
   useEffect(() => {
-    /*    const first = firebase.firestore.collection("children")
-            .orderBy("lastName")
-            .limit(rowsPerPage);
-            first.get()
-            .then((documentSnapshots) => {
-              setData(first)
-              setLastVisible(documentSnapshots.docs[documentSnapshots.docs.length-1])
-            })
-            .catch(function (error) {
-              console.error(error);
-            })
-            */
+    const firstQuery = firebase.firestore.collection("children")
+      .orderBy("lastName")
+      .limit(rowsPerPage);
+
+    firstQuery.get()
+      .then((snapshots) => {
+        console.log(snapshots);
+        if (!snapshots.empty) {
+          const documents: Array<firebase.firestore.DocumentData> = [];
+          snapshots.docs.map((doc) => {
+            const document = doc.data();
+            documents.push(document);
+          });
+          setData(documents);
+          console.log(documents);
+          setLastVisible(snapshots.docs[snapshots.docs.length - 1])
+        } else {
+          setData([]);
+        }
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+    /*
     const visibleData = exampleData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
     setData(visibleData);
     setItemsCount(exampleData.length);
+    */
   }, []);
 
-  const rowsPerPageChangeHandler = (event: any) => {
+  /*const rowsPerPageChangeHandler = (event: any) => {
     setPage(0);
     const newRowsPerPage = parseInt(event.target.value, 10)
     const visibleData = exampleData.slice(0 * newRowsPerPage, 0 * newRowsPerPage + newRowsPerPage);
     setRowsPerPage(newRowsPerPage);
     setData(visibleData);
-  } 
+  }
+  */
 
-  const pageChangeHandler = (event: any, newPage: number) => {
-    const visibleData = exampleData.slice(newPage * rowsPerPage, newPage * rowsPerPage + rowsPerPage);
-    setPage(newPage);
-    setData(visibleData);
-  };
-  if (data) {
+  const getData = (query: firebase.firestore.Query<firebase.firestore.DocumentData>) => {
+    query.get()
+      .then((snapshots) => {
+        console.log(snapshots);
+        if (!snapshots.empty) {
+          const documents: Array<firebase.firestore.DocumentData> = [];
+          snapshots.docs.map((doc) => {
+            const document = doc.data();
+            documents.push(document);
+          });
+          setData(documents);
+          console.log(documents);
+          setLastVisible(snapshots.docs[snapshots.docs.length - 1])
+        } else {
+          setData([]);
+        }
+      })
+  }
+
+  const nextPageChangeHandler = () => {
+    const query = firebase.firestore.collection("children")
+      .orderBy("lastName")
+      .limit(rowsPerPage)
+      .startAfter(lastVisible)
+    getData(query);
+    const newPage = page + 1;
+    setPage(newPage)
+  }
+
+  const previousPageChangeHandler = () => {
+    const query = firebase.firestore.collection("children")
+      .orderBy("lastName")
+      .limit(rowsPerPage)
+      .endBefore(lastVisible);
+    getData(query);
+    const newPage = page - 1;
+    setPage(newPage)
+  }
+
+  /*  const pageChangeHandler = (event: any, newPage: number) => {
+      const visibleData = exampleData.slice(newPage * rowsPerPage, newPage * rowsPerPage + rowsPerPage);
+      setPage(newPage);
+      setData(visibleData);
+    };
+    */
+
+  if (data.length !== 0) {
     return (
       <Container >
         <Typography variant="h2" align="center" gutterBottom >
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Child Name</TableCell>
-                {data[0].results.map((elem: any) => {
-                  return (
-                    <TableCell>{elem.title} score</TableCell>
-                  )
-                })}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {data.map((elem: any) => {
+          Results
+      </Typography>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Child Name</TableCell>
+              {data[0].results.map((elem: any) => {
                 return (
-                  <TableRow>
-                    <TableCell>{`${elem.firstName} ${elem.lastName}`}</TableCell>
-                    {elem.results.map((result: any) => {
-                      return (
-                        <TableCell>{result.points}</TableCell>
-                      )
-                    })}
-                  </TableRow>
+                  <TableCell>{elem.title} score</TableCell>
                 )
               })}
-            </TableBody>
-            <TableFooter>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((elem: any) => {
+              return (
+                <TableRow>
+                  <TableCell>{`${elem.firstName} ${elem.lastName}`}</TableCell>
+                  {elem.results.map((result: any) => {
+                    return (
+                      <TableCell>{result.points}</TableCell>
+                    )
+                  })}
+                </TableRow>
+              )
+            })}
+          </TableBody>
+          {/*          <TableFooter>
+            <TableRow>
               <TablePagination
-                count={itemsCount}
+                count={-1}
                 onChangePage={pageChangeHandler}
                 rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
                 rowsPerPage={rowsPerPage}
                 page={page}
                 onChangeRowsPerPage={rowsPerPageChangeHandler}
               />
-            </TableFooter>
-          </Table>
-        </Typography>
+            </TableRow>
+</TableFooter>*/}
+        </Table>
+        <div>
+          <IconButton onClick={previousPageChangeHandler}><ArrowBackIosIcon /></IconButton>
+          <IconButton onClick={nextPageChangeHandler}><ArrowForwardIosIcon /></IconButton>
+        </div>
       </Container>
     )
-  }
-  else {
-    return <h3>loading</h3>
+  } else {
+    return (<h3>No results</h3>)
   }
 }
 
