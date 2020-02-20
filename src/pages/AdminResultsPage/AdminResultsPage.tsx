c00d65d03391c953111c99e534dafa3af6d7a286
Paweł Tuliński <pawel.tulinski@gmail.com>
2020-02-20 17:04:41 +0100

    Remove placeholder data

--- src/pages/AdminResultsPage/AdminResultsPage.tsx
+++ src/pages/AdminResultsPage/AdminResultsPage.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Typography, Table, TableBody, TableHead, TableRow, TableCell, TableFooter, TablePagination, IconButton } from '@material-ui/core/';
+import { Container, Typography, Table, TableBody, TableHead, TableRow, TableCell, IconButton } from '@material-ui/core/';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import i18next from "i18next"; // ADD LATER
 import { firebase } from '../../firebase/Firebase';
-import exampleData from './exampleData';
 
 const AdminResultsPage = () => {
   const [data, setData] = useState<Array<firebase.firestore.DocumentData>>([]);
@@ -12,13 +11,13 @@ const AdminResultsPage = () => {
   const [rowsPerPage, setRowsPerPage] = useState(10);
   const [lastVisible, setLastVisible] = useState();
   useEffect(() => {
-    const firstQuery = firebase.firestore.collection("children")
+    const firstQuery = firebase.firestore
+      .collection("children")
       .orderBy("lastName")
       .limit(rowsPerPage);
 
     firstQuery.get()
       .then((snapshots) => {
-        console.log(snapshots);
         if (!snapshots.empty) {
           const documents: Array<firebase.firestore.DocumentData> = [];
           snapshots.docs.map((doc) => {
@@ -26,7 +25,6 @@ const AdminResultsPage = () => {
             documents.push(document);
           });
           setData(documents);
-          console.log(documents);
           setLastVisible(snapshots.docs[snapshots.docs.length - 1])
         } else {
           setData([]);
@@ -35,26 +33,11 @@ const AdminResultsPage = () => {
       .catch(function (error) {
         console.error(error);
       });
-    /*
-    const visibleData = exampleData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
-    setData(visibleData);
-    setItemsCount(exampleData.length);
-    */
   }, []);
-
-  /*const rowsPerPageChangeHandler = (event: any) => {
-    setPage(0);
-    const newRowsPerPage = parseInt(event.target.value, 10)
-    const visibleData = exampleData.slice(0 * newRowsPerPage, 0 * newRowsPerPage + newRowsPerPage);
-    setRowsPerPage(newRowsPerPage);
-    setData(visibleData);
-  }
-  */
 
   const getData = (query: firebase.firestore.Query<firebase.firestore.DocumentData>) => {
     query.get()
       .then((snapshots) => {
-        console.log(snapshots);
         if (!snapshots.empty) {
           const documents: Array<firebase.firestore.DocumentData> = [];
           snapshots.docs.map((doc) => {
@@ -62,16 +45,19 @@ const AdminResultsPage = () => {
             documents.push(document);
           });
           setData(documents);
-          console.log(documents);
           setLastVisible(snapshots.docs[snapshots.docs.length - 1])
         } else {
           setData([]);
         }
       })
+      .catch(function (error) {
+        console.error(error);
+      });
   }
 
   const nextPageChangeHandler = () => {
-    const query = firebase.firestore.collection("children")
+    const query = firebase.firestore
+      .collection("children")
       .orderBy("lastName")
       .limit(rowsPerPage)
       .startAfter(lastVisible)
@@ -81,7 +67,8 @@ const AdminResultsPage = () => {
   }
 
   const previousPageChangeHandler = () => {
-    const query = firebase.firestore.collection("children")
+    const query = firebase.firestore
+      .collection("children")
       .orderBy("lastName")
       .limit(rowsPerPage)
       .endBefore(lastVisible);
@@ -89,13 +76,6 @@ const AdminResultsPage = () => {
     const newPage = page - 1;
     setPage(newPage)
   }
-
-  /*  const pageChangeHandler = (event: any, newPage: number) => {
-      const visibleData = exampleData.slice(newPage * rowsPerPage, newPage * rowsPerPage + rowsPerPage);
-      setPage(newPage);
-      setData(visibleData);
-    };
-    */
 
   if (data.length !== 0) {
     return (
@@ -128,27 +108,19 @@ const AdminResultsPage = () => {
               )
             })}
           </TableBody>
-          {/*          <TableFooter>
-            <TableRow>
-              <TablePagination
-                count={-1}
-                onChangePage={pageChangeHandler}
-                rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onChangeRowsPerPage={rowsPerPageChangeHandler}
-              />
-            </TableRow>
-</TableFooter>*/}
         </Table>
         <div>
-          <IconButton onClick={previousPageChangeHandler}><ArrowBackIosIcon /></IconButton>
+          <IconButton disabled={page === 0 ? true : false} onClick={previousPageChangeHandler}><ArrowBackIosIcon /></IconButton>
           <IconButton onClick={nextPageChangeHandler}><ArrowForwardIosIcon /></IconButton>
         </div>
       </Container>
     )
   } else {
-    return (<h3>No results</h3>)
+    return (
+      <Typography variant="h3" align="center" gutterBottom>
+        No results
+      </Typography>
+    )
   }
 }
 
