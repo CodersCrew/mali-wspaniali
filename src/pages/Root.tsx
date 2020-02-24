ac5c6d0a229a75c1eff9de1c1c5f062995bf92e1
Adam Oleszko <adamoleszko44@gmail.com>
2020-02-24 19:48:30 +0100

    Modify languages component

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -4,12 +4,12 @@ import { HomePage } from './HomePage';
 import { LoginPage } from './LoginPage';
 import { RegistrationPage } from './RegistrationPage';
 import { Loader } from '../components/Loader';
-import { ChangeLanguage } from '../components/ChangeLanguage';
+import { LanguageSelector } from '../components/LanguageSelector';
 
 export const Root = () => {
   return (
     <Suspense fallback={<Loader />}>
-      <ChangeLanguage />
+      <LanguageSelector />
       <Router>
         <div>
           <Route exact path="/" component={HomePage} />
