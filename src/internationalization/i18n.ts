a23cb7869a1c1249389b5dda15ab38eb4f1df819
Adam Oleszko <adamoleszko44@gmail.com>
2020-02-25 19:25:03 +0100

    Remove german translation

--- src/internationalization/i18n.ts
+++ src/internationalization/i18n.ts
@@ -1,7 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import translationDe from './locales/de/translation.json';
 import translationEn from './locales/en/translation.json';
 import translationPl from './locales/pl/translation.json';
 
@@ -16,9 +15,6 @@ i18n
       },
       en: {
         translation: translationEn,
-      },
-      de: {
-        translation: translationDe,
       },
     },
     interpolation: {
