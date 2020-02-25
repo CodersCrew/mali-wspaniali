8687153ab6aa47472f27ee96b3dbe8a0a1afdcec
Paweł Tuliński <pawel.tulinski@gmail.com>
2020-02-25 22:48:50 +0100

    eslint fixes

--- src/firebase/childRepository.ts
+++ src/firebase/childRepository.ts
@@ -3,48 +3,47 @@ import 'firebase/firestore';
 
 export const childRepository = (db: firebaseApp.firestore.Firestore) => ({
   getChildrenFirstPage: (rowsPerPage: number) => {
-    let documents: firebaseApp.firestore.DocumentData[] = [];
-    let lastVisible: firebaseApp.firestore.DocumentData[] = [];
-    const unsubscribe = () => db.collection("child")
-      .orderBy("lastName")
+    const documents: firebaseApp.firestore.DocumentData[] = [];
+    const lastVisible: firebaseApp.firestore.DocumentData[] = [];
+    const unsubscribe = () => db.collection('child')
+      .orderBy('lastName')
       .limit(rowsPerPage)
       .onSnapshot(
         { includeMetadataChanges: true },
         (snapshots) => {
-          snapshots.docs.forEach((doc) => documents.push(doc.data()))
-          lastVisible.push(snapshots.docs[snapshots.docs.length - 1])
-        })
+          snapshots.docs.forEach((doc) => documents.push(doc.data()));
+          lastVisible.push(snapshots.docs[snapshots.docs.length - 1]);
+        });
     return { documents, unsubscribe, lastVisible };
   },
   getChildrenNextPage: (rowsPerPage: number, previousLastVisible: firebaseApp.firestore.DocumentData) => {
-    let documents: firebaseApp.firestore.DocumentData[] = [];
-    let newLastVisible: firebaseApp.firestore.DocumentData[] = [];
-    const unsubscribe = () => db.collection("child")
-      .orderBy("lastName")
+    const documents: firebaseApp.firestore.DocumentData[] = [];
+    const newLastVisible: firebaseApp.firestore.DocumentData[] = [];
+    const unsubscribe = () => db.collection('child')
+      .orderBy('lastName')
       .limit(rowsPerPage)
       .startAfter(previousLastVisible)
       .onSnapshot(
         { includeMetadataChanges: true },
         (snapshots) => {
-          snapshots.docs.forEach((doc) => documents.push(doc.data()))
-          newLastVisible.push(snapshots.docs[snapshots.docs.length - 1])
-        })
-    return { documents, unsubscribe, newLastVisible }
+          snapshots.docs.forEach((doc) => documents.push(doc.data()));
+          newLastVisible.push(snapshots.docs[snapshots.docs.length - 1]);
+        });
+    return { documents, unsubscribe, newLastVisible };
   },
   getChildrenPrevPage: (rowsPerPage: number, previousLastVisible: firebaseApp.firestore.DocumentData) => {
-    let documents: firebaseApp.firestore.DocumentData[] = [];
-    let newLastVisible: firebaseApp.firestore.DocumentData[] = [];
-    const unsubscribe = () => db.collection("child")
-      .orderBy("lastName")
+    const documents: firebaseApp.firestore.DocumentData[] = [];
+    const newLastVisible: firebaseApp.firestore.DocumentData[] = [];
+    const unsubscribe = () => db.collection('child')
+      .orderBy('lastName')
       .limit(rowsPerPage)
       .endBefore(previousLastVisible)
       .onSnapshot(
         { includeMetadataChanges: true },
         (snapshots) => {
-          console.log(snapshots);
-          snapshots.docs.forEach((doc) => documents.push(doc.data()))
-          newLastVisible.push(snapshots.docs[snapshots.docs.length - 1])
-        })
-    return { documents, unsubscribe, newLastVisible }
+          snapshots.docs.forEach((doc) => documents.push(doc.data()));
+          newLastVisible.push(snapshots.docs[snapshots.docs.length - 1]);
+        });
+    return { documents, unsubscribe, newLastVisible };
   }
-})
+});
