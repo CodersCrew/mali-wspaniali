9bc88c594fada2921ba9021cda3e9d036cd589eb
KamilaCoder <kamila.spodymek@gmail.com>
2020-03-24 20:09:48 +0100

    add basic react-quill component

--- src/pages/Newsletter/Newsletter.tsx
+++ src/pages/Newsletter/Newsletter.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 export const Newsletter = () => {
 
     return (
-        <h1>Newsletter</h1>
+        <WorkSpace />
     );
 };
+
+
+const WorkSpace = () => {
+
+    const [ text, setText ] = useState('');
+    
+    const handleChange = (value:any) => {
+      setText(value);
+    };
+  
+      return (
+        <ReactQuill value={text}
+            onChange={handleChange} />
+      );
+  };
--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -8,6 +8,7 @@ import { ChildProfile } from './ChildProfile';
 import { ParentProfile } from './ParentProfile';
 import { LanguageSelector } from '../components/LanguageSelector';
 import { UsersPage } from './UsersPage';
+import { Newsletter } from './Newsletter';
 
 export const Root = () => {
   return (
@@ -24,6 +25,7 @@ export const Root = () => {
           <Route path="/admin" />
           <Route path="/admin/parent/:id" component={ParentProfile} />
           <Route path="/parent" />
+          <Route path="/admin/newsletter" component={Newsletter} />
         </div>
       </Router>
     </>
