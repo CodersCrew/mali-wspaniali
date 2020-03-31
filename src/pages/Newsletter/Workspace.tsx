afb4e0bbcede7a339b832de6039c3362e37032be
KamilaCoder <kamila.spodymek@gmail.com>
2020-03-31 11:31:15 +0200

    move static ract-quill config to workspaceConfig.ts

--- src/pages/Newsletter/Workspace.tsx
+++ src/pages/Newsletter/Workspace.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import { useTranslation } from 'react-i18next';
+import { modules, formats } from './workspaceConfig';
 import { WorkspaceProps, Message } from './types';
-
 
 export const WorkSpace = (props: WorkspaceProps) => {
 
@@ -12,30 +12,6 @@ export const WorkSpace = (props: WorkspaceProps) => {
     const handleChange = (value: Message) => {
         props.setMessage(value);
     };
-
-    const modules = {
-        toolbar: [
-            [{ 'header': [1, 2, 3, 4, false] }],
-            [{ 'font': [] }],
-            ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-            [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
-            [{ 'align': [] }],
-            [{ 'color': [] }, { 'background': [] }],
-            [{ 'script': 'sub' }, { 'script': 'super' }],
-            ['link', 'image', 'video'],
-            ['clean']
-        ],
-        imageResize: {
-            modules: ['Resize', 'DisplaySize', 'Toolbar']
-        }
-    };
-
-    const formats = [
-        'header',
-        'bold', 'italic', 'underline', 'strike', 'blockquote',
-        'list', 'bullet', 'indent',
-        'link', 'image'
-    ];
 
     return (
         <ReactQuill value={props.message} onChange={handleChange} theme="snow" modules={modules}
---
+++ src/pages/Newsletter/workspaceConfig.ts
@@ -0,0 +1,23 @@
+export const modules = {
+    toolbar: [
+        [{ 'header': [1, 2, 3, 4, false] }],
+        [{ 'font': [] }],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+        [{ 'align': [] }],
+        [{ 'color': [] }, { 'background': [] }],
+        [{ 'script': 'sub' }, { 'script': 'super' }],
+        ['link', 'image', 'video'],
+        ['clean']
+    ],
+    imageResize: {
+        modules: ['Resize', 'DisplaySize', 'Toolbar']
+    }
+};
+
+export const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+];
