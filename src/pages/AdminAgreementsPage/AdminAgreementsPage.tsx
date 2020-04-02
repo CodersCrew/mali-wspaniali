f9d33870887dd83e12c78f676b9c1a2bac0b6180
Dominikosseqq <domminots@gmail.com>
2020-04-02 17:40:54 +0200

    add prettier
    prettier -> all files

--- src/pages/AdminAgreementsPage/AdminAgreementsPage.tsx
+++ src/pages/AdminAgreementsPage/AdminAgreementsPage.tsx
@@ -64,7 +64,7 @@ export const AdminAgreementsPage = () => {
 
   useEffect(() => {
     load(waitForAgreementsData());
-    return () => detachListeners();
+    return () => detachListeners(); // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleToggle = (id: string) => () => {
