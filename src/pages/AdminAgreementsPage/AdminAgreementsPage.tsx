31b1f736c619f43fe5dccbdaad8433e1b6541fd0
Dominikosseqq <domminots@gmail.com>
2020-04-03 20:24:17 +0200

    added prettier

--- src/pages/AdminAgreementsPage/AdminAgreementsPage.tsx
+++ src/pages/AdminAgreementsPage/AdminAgreementsPage.tsx
@@ -19,23 +19,22 @@ import {
 } from '@material-ui/core';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    paper: {
-      position: 'absolute',
-      width: 400,
-      backgroundColor: theme.palette.background.paper,
-      border: '2px solid #000',
-      boxShadow: theme.shadows[5],
-      padding: theme.spacing(2, 4, 3),
-    },
-  }),
-);
-
 export const AdminAgreementsPage = () => {
   useAuthorization(true, '/', ['admin']);
-  const { t } = useTranslation();
+  const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+      paper: {
+        position: 'absolute',
+        width: 400,
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+      },
+    }),
+  );
   const classes = useStyles();
+  const { t } = useTranslation();
   const [agreementsList, setAgreementsList] = useState<AdminAgreement[]>();
   const [listeners, setListeners] = useState<(() => void)[]>([]);
   const [checked, setChecked] = useState<string[]>([]);
