204ba09871c47d8fb66282bd5d4beff41c5b03d9
Dominikosseqq <domminots@gmail.com>
2020-04-04 14:51:16 +0200

    before update from master

--- src/firebase/types.ts
+++ src/firebase/types.ts
@@ -1,5 +1,27 @@
 export type Document = firebase.firestore.DocumentData;
 
+export interface Article {
+  articleId: string;    
+  titles: string[];      //[1]short[2]long
+  content: string[];    //[n]header[n+1]content
+  tags: string[];
+  category: string;
+  description: string;
+  videoUrl: string;        //last video yt bottom
+  readingTime: number;
+  redactor: Redactor;
+  date: Date;
+}
+
+export interface Redactor {
+  userId: string;
+  firstName: string;
+  lastName: string;
+  profession: string;
+  photoUrl: string;
+  shortDescription: string;
+}
+
 export interface Result {
   title: string;
   description: string;
