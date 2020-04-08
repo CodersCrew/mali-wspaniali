5b9cc181480467f4ab492dd1e1da5007612b9682
Paweł Tuliński <pawel.tulinski@gmail.com>
2020-04-08 16:33:19 +0200

    Fix prettier issues

--- src/queries/httpQueries.ts
+++ src/queries/httpQueries.ts
@@ -1,10 +1,8 @@
 import firebaseApp from 'firebase/app';
 
-export const getDatabaseBackup = firebaseApp
-  .functions()
-  .httpsCallable('cloudFunctions-databaseBackup');
+export const getDatabaseBackup = firebaseApp.functions().httpsCallable('cloudFunctions-databaseBackup');
 
 export const getStorageRef = (url: string) => {
-  const fileRef = firebaseApp.storage().refFromURL(url);
-  return fileRef;
+    const fileRef = firebaseApp.storage().refFromURL(url);
+    return fileRef;
 };
