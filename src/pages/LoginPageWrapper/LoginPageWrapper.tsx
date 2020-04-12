3b045d7dad520ef15da1ceb85ed1d8b1d5b452b0
Krzysiek Karczewski <krzysztofkarczewski92@gmail.com>
2020-04-12 16:11:07 +0200

    add welcome text under the logo

--- src/pages/LoginPageWrapper/LoginPageWrapper.tsx
+++ src/pages/LoginPageWrapper/LoginPageWrapper.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core';
+import { useTranslation } from 'react-i18next';
 import { mainColor, backgroundColor } from '../../colors';
 import Logo from '../../assets/MALWSP_logo.png';
 
 export const LoginPageWrapper: React.FC = ({ children }) => {
     const classes = useStyles();
+    const { t } = useTranslation();
 
     return (
         <div className={classes.background}>
             <div className={classes.logoContainer}>
                 <img src={Logo} alt="Mali Wspaniali Logo"></img>
+                <div className={classes.welcomeText}>{t('login-wrapper.welcome-text')}</div>
             </div>
             <div className={classes.formContainer}>{children}</div>
         </div>
@@ -29,6 +32,7 @@ const useStyles = makeStyles({
         alignItems: 'center',
         justifyContent: 'center',
         flex: '2 0 0',
+        flexDirection: 'column',
     },
     formContainer: {
         backgroundColor,
@@ -37,4 +41,16 @@ const useStyles = makeStyles({
         borderRadius: '10px',
         flex: '1 0 0'
     },
+    welcomeText: {
+        marginTop: '50px',
+        color: backgroundColor,
+        fontStyle: 'normal',
+        fontWeight: 'bold',
+        fontSize: '36px',
+        lineHeight: '44px',
+        display: 'flex',
+        alignItems: 'center',
+        textAlign: 'center',
+        textTransform: 'uppercase',
+    }
 });
