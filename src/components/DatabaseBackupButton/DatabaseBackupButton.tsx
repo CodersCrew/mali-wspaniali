124e1560aac1879210236cc9cc41b6be75df0932
Paweł Tuliński <pawel.tulinski@gmail.com>
2020-04-13 12:19:45 +0200

    Add AlertDialog and fix translations

--- src/components/DatabaseBackupButton/DatabaseBackupButton.tsx
+++ src/components/DatabaseBackupButton/DatabaseBackupButton.tsx
@@ -4,49 +4,47 @@ import Button from '@material-ui/core/Button';
 import { getDatabaseBackup, getStorageRef } from '../../queries/httpQueries';
 import { getCurrentUserIdToken } from '../../queries/userQueries';
 import { load } from '../../utils/load';
+import { openAlertDialog } from '../AlertDialog';
 
 export const DatabaseBackupButton = () => {
-  // Temporary solution until admin page is up
-  const isProd = false;
+    // Temporary solution until admin page is up
+    const isProd = true;
 
-  const { t } = useTranslation();
+    const { t } = useTranslation();
 
-  const getBackup = () => {
-    load(
-      getDatabaseBackup().then(async response => {
-        const fileName = response.data.fileName;
-        const fileRef = getStorageRef(response.data.backupUrl);
-        const [idToken, fileUrl] = await Promise.all([
-          getCurrentUserIdToken(),
-          fileRef.getDownloadURL(),
-        ]);
-        fetch(fileUrl, {
-          method: 'GET',
-          headers: {
-            Authorization: `Bearer ${idToken}`,
-          },
-        })
-          .then(response => {
-            response.blob().then(blob => {
-              let url = window.URL.createObjectURL(blob);
-              let a = document.createElement('a');
-              a.href = url;
-              a.download = fileName;
-              a.click();
-            });
+    const getBackup = () => {
+        load(
+            getDatabaseBackup().then(async response => {
+                const fileName = response.data.fileName;
+                const fileRef = getStorageRef(response.data.backupUrl);
+                const [idToken, fileUrl] = await Promise.all([getCurrentUserIdToken(), fileRef.getDownloadURL()]);
+                fetch(fileUrl, {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${idToken}`,
+                    },
+                })
+                    .then(response => {
+                        response.blob().then(blob => {
+                            let url = window.URL.createObjectURL(blob);
+                            let a = document.createElement('a');
+                            a.href = url;
+                            a.download = fileName;
+                            a.click();
+                        });
+                    })
+            }).catch(error => {
+              console.log(error);
+              openAlertDialog({ type: 'error', description: error.message });
           })
-          .catch(error => {
-            console.log(error);
-          });
-      }),
-    );
-  };
+        );
+    };
 
-  if (isProd)
-    return (
-      <Button variant="contained" color="primary" onClick={getBackup}>
-        {t('database-backup.get-backup-json')}
-      </Button>
-    );
-  return null;
+    if (isProd)
+        return (
+            <Button variant="contained" color="primary" onClick={getBackup}>
+                {t('database-backup.get-backup-json')}
+            </Button>
+        );
+    return null;
 };
