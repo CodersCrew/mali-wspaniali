cd6237088207aaa967b3d9431d66d8275dcb7afc
KamilaCoder <kamila.spodymek@gmail.com>
2020-04-14 16:59:20 +0200

    cleanup

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import { Button, Container, makeStyles } from '@material-ui/core/';
 import { useTranslation } from 'react-i18next';
 import { firebase } from '../../firebase/firebase';
-import { Navbar } from '../../components/Navbar/Navbar';
 
 export const HomePage = () => {
     const classes = useStyles();
@@ -15,31 +14,28 @@ export const HomePage = () => {
     };
 
     return (
-        <>
-            <Navbar></Navbar>
-            <Container className={classes.container}>
-                <Link to="/">
-                    <Button onClick={handleLogoutClick} color="secondary" variant="outlined">
-                        {t('home-page.log-out')}
-                    </Button>
-                </Link>
-                <Link to="/login">
-                    <Button variant="contained" color="primary">
-                        {t('home-page.go-to-login-page')}
-                    </Button>
-                </Link>
-                <Link to="/register">
-                    <Button variant="contained" color="primary">
-                        {t('home-page.go-to-registration-page')}
-                    </Button>
-                </Link>
-                <Link to="/parent/children">
-                    <Button variant="contained" color="primary">
-                        {t('home-page.go-to-parent-children-page')}
-                    </Button>
-                </Link>
-            </Container>
-        </>
+        <Container className={classes.container}>
+            <Link to="/">
+                <Button onClick={handleLogoutClick} color="secondary" variant="outlined">
+                    {t('home-page.log-out')}
+                </Button>
+            </Link>
+            <Link to="/login">
+                <Button variant="contained" color="primary">
+                    {t('home-page.go-to-login-page')}
+                </Button>
+            </Link>
+            <Link to="/register">
+                <Button variant="contained" color="primary">
+                    {t('home-page.go-to-registration-page')}
+                </Button>
+            </Link>
+            <Link to="/parent/children">
+                <Button variant="contained" color="primary">
+                    {t('home-page.go-to-parent-children-page')}
+                </Button>
+            </Link>
+        </Container>
     );
 };
 
