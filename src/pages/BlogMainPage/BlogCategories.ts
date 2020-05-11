64ee30b997dd8e5afb6d1ca3dad07881bd51c8a2
KamilaCoder <kamila.spodymek@gmail.com>
2020-05-11 20:51:41 +0200

    fix blog category translations

--- src/pages/BlogMainPage/BlogCategories.ts
+++ src/pages/BlogMainPage/BlogCategories.ts
@@ -1,14 +1,11 @@
 import i18n from 'i18next';
 
-const {t} = i18n;
- 
 export const categories = {
-  
-    all: { name: t('blog-categories.all'), color: 'orange' },
-    food: { name: t('blog-categories.food'), color: 'yellow' },
-    activity: { name: t('blog-categories.physical-activity'), color: 'purple' },
-    emotions: { name: t('blog-categories.emotions'), color: 'lightOrange' },
-    other: { name: t('blog-categories.other'), color: 'blue' }
+    all: { name: i18n.t('blog-categories.all'), color: 'orange' },
+    food: { name: i18n.t('blog-categories.food'), color: 'yellow' },
+    activity: { name: i18n.t('blog-categories.physical-activity'), color: 'purple' },
+    emotions: { name: i18n.t('blog-categories.emotions'), color: 'lightOrange' },
+    other: { name: i18n.t('blog-categories.other'), color: 'blue' }
 };
   
 export const categoriesList = Object.entries(categories).map(([key, value]) => {
