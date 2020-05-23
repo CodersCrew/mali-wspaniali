c8cf1f43974a9c3d3cc2b83bae1a22338a61e50e
Adam Oleszko <adamoleszko44@gmail.com>
2020-05-23 15:23:03 +0200

    Add prettierignore

--- functions/src/repository/authRepository.ts
+++ functions/src/repository/authRepository.ts
@@ -1,11 +1,11 @@
 import * as admin from 'firebase-admin';
 
 export const authRepository = (userId: string) => ({
-  setParentRole: () => {
-    return admin
-      .firestore()
-      .collection('user')
-      .doc(userId)
-      .update({ role: 'parent' });
-  },
+    setParentRole: () => {
+        return admin
+            .firestore()
+            .collection('user')
+            .doc(userId)
+            .update({ role: 'parent' });
+    },
 });
