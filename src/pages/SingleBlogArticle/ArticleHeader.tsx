ba462d83ad563c9b6c052175132ff47544985754
Krzysiek Karczewski <krzysztofkarczewski92@gmail.com>
2020-05-31 12:29:50 +0200

    add mobile styles for article's header

--- src/pages/SingleBlogArticle/ArticleHeader.tsx
+++ src/pages/SingleBlogArticle/ArticleHeader.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles, createStyles, Grid, Typography } from '@material-ui/core';
-import { mainColor } from '../../colors';
+import { mainColor, textColor } from '../../colors';
 import { lineHeight, letterSpace } from '../../fontStyle';
 
 export const ArticleHeader = ({ title }: { title: string }) => {
@@ -8,7 +8,7 @@ export const ArticleHeader = ({ title }: { title: string }) => {
 
     return (
         <Grid className={classes.headerLongTitle} item xs={10}>
-            <Typography className={classes.headerLongTitleText}>{title.toUpperCase()}</Typography>
+            <Typography className={classes.headerLongTitleText}>{title}</Typography>
         </Grid>
     );
 };
@@ -18,13 +18,23 @@ const useStyles = makeStyles(() =>
         headerLongTitle: {
             paddingTop: '3vw',
             paddingBottom: '5vw',
+            '@media (max-width:767px)': {
+                margin: '20px 40px 50px 15px',
+            },
         },
         headerLongTitleText: {
             fontSize: '34px',
             color: mainColor,
             fontWeight: 'bold',
+            textTransform: 'uppercase',
             letterSpacing: letterSpace,
             lineHeight,
+
+            '@media (max-width:767px)': {
+                fontSize: '21px',
+                color: textColor,
+                textTransform: 'initial',
+            },
         },
     }),
 );
