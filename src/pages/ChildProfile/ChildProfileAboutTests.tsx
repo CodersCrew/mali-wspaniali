2cf558d03b14638a463912a3a99c02c60e7e4723
KonradSzwarc <konrad.szwarc.dev@gmail.com>
2020-06-06 14:47:29 +0200

    wip

--- src/pages/ChildProfile/ChildProfileAboutTests.tsx
+++ src/pages/ChildProfile/ChildProfileAboutTests.tsx
@@ -1,3 +1,22 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-export const ChildProfileAboutTests = () => <div>ABOUT TESTS</div>;
+export const ChildProfileAboutTests = () => {
+    const classes = useStyles();
+    const { t } = useTranslation();
+
+    return <Card className={classes.card}></Card>;
+};
+
+const useStyles = makeStyles({
+    card: {
+        padding: 40,
+    },
+});
