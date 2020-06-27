57af8333f0fca70b162b257ccd81ef612aeacdc6
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-06-27 18:19:11 +0200

    [sdk] add article resolvers for all, single and create

--- server/src/articles/articles.module.ts
+++ server/src/articles/articles.module.ts
@@ -3,11 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { ArticlesResolver } from './articles.resolver';
 import { ArticleSchema } from './schemas/articles.schema';
+import { ArticleService } from './articles.service';
+import { ArticlesRepository } from './domain/repositories/article_repository';
 
 @Module({
-  providers: [ArticlesResolver],
   imports: [
     MongooseModule.forFeature([{ name: 'Article', schema: ArticleSchema }]),
   ],
+  providers: [ArticlesResolver, ArticleService, ArticlesRepository],
 })
 export class ArticlesModule {}
