7049e9e01e2f439d74ec972fa7ace801a0d87aeb
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-06-27 18:01:50 +0200

    [sdk] add mongoDB article schema

---
+++ server/src/articles/schemas/articles.schema.ts
@@ -0,0 +1,20 @@
+import * as mongoose from 'mongoose';
+import { RedactorSchema } from './redactor.schema';
+
+export const ArticleSchema = new mongoose.Schema({
+  category: [String],
+  contentHTML: String,
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+  description: String,
+  header: String,
+  pictureUrl: String,
+  readingTime: Number,
+  redactor: RedactorSchema,
+  subtitle: String,
+  tags: [String],
+  title: String,
+  videoUrl: String,
+});
