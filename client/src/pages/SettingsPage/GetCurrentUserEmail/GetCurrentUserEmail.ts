a2d759bf5b1b551fdef4355b55dc536d2049b035
Marek Kustosz <markust71@hotmail.com>
2020-06-28 22:25:06 +0200

    MALWSP-295: FormControlOldPassword extracted

---
+++ client/src/pages/SettingsPage/GetCurrentUserEmail/GetCurrentUserEmail.ts
@@ -0,0 +1,10 @@
+import { firebase } from '../../../firebase/firebase';
+
+export const getCurrentUserEmail = () => {
+    const currentUser = firebase.auth.getCurrentUser();
+    let ret;
+    if (currentUser) {
+        ret = currentUser.email;
+    }
+    return ret;
+};
