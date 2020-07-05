6d00948304c42dadcd470d3c9ca0357c56ed5398
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-07-05 17:47:19 +0200

    [sdk] add sentry, env to project

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import * as Sentry from '@sentry/node';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  Sentry.init({
+    dsn: process.env.SENTRY_API_KEY,
+  });
+
   await app.listen(process.env.PORT || 3005);
 }
 bootstrap();
