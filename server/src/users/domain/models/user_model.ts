6feffd096453900df7b383a97974f1402b810ee0
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-07-09 23:58:03 +0200

    [sdk] do not expose password, validate user before creation

--- server/src/users/domain/models/user_model.ts
+++ server/src/users/domain/models/user_model.ts
@@ -1,18 +1,37 @@
 import { AggregateRoot } from '@nestjs/cqrs';
 import { UserCreatedEvent } from '../events/impl/article_created_event';
+import { Mail } from './mail';
+
 export interface UserProps {
   readonly _id: string;
   readonly date: Date;
-  readonly email: string;
+  mail: string;
+  readonly password: string;
+}
+
+export interface UserBeforeSaveProps {
+  mail: string;
   readonly password: string;
 }
 
 export class User extends AggregateRoot {
-  constructor(private readonly props: UserProps) {
+  constructor(private readonly props: UserProps | UserBeforeSaveProps) {
     super();
+
+    this.props.mail = Mail.create(props.mail).getValue().value;
   }
 
   removeKeyCode(keyCode: string): void {
-    this.apply(new UserCreatedEvent(this.props._id, keyCode));
+    if (isUserProps(this.props)) {
+      this.apply(new UserCreatedEvent(this.props._id, keyCode));
+    }
   }
+
+  getProps(): UserProps | UserBeforeSaveProps {
+    return this.props;
+  }
+}
+
+function isUserProps(v: UserProps | UserBeforeSaveProps): v is UserProps {
+  return !!(v as UserProps)._id;
 }
