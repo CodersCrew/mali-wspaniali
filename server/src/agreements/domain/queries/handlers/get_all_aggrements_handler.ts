21ca538d54a031635269d74cbf33fc7da95b92cb
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-07-22 00:15:11 +0200

    [sdk] add queries

---
+++ server/src/agreements/domain/queries/handlers/get_all_aggrements_handler.ts
@@ -0,0 +1,15 @@
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+
+import { AggrementRepository } from '../../repositories/aggrement_repository';
+import { GetAllAggrementsQuery } from '../impl/get_all_aggrements_query';
+import { AggrementProps } from '../../../../agreements/schemas/aggrement_schema';
+
+@QueryHandler(GetAllAggrementsQuery)
+export class GetAllAggrementsHandler
+  implements IQueryHandler<GetAllAggrementsQuery> {
+  constructor(private readonly repository: AggrementRepository) {}
+
+  async execute(query: GetAllAggrementsQuery): Promise<AggrementProps[]> {
+    return this.repository.getAll();
+  }
+}
---
+++ server/src/agreements/domain/queries/impl/get_all_aggrements_query.ts
@@ -0,0 +1,3 @@
+export class GetAllAggrementsQuery {
+  constructor() {}
+}
