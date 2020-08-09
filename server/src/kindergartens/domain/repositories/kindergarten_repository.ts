db97800dbd312217cde117f206e33459e1bc4e76
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-08-09 21:50:23 +0200

    kindergarten: add schema, repository

---
+++ server/src/kindergartens/domain/repositories/kindergarten_repository.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { KindergartenProps } from '../models/kindergarten_model';
+import { KindergartenDocument } from '../../schemas/kindergarten_schema';
+import { CreateKindergartenInput } from '../../inputs/create_kindergarten_input';
+
+@Injectable()
+export class KindergartenRepository {
+  constructor(
+    @InjectModel('Kindergarten')
+    private readonly model: Model<KindergartenDocument>,
+  ) {}
+
+  async getAll(): Promise<KindergartenProps[]> {
+    return await this.model.find({}, {}, { sort: { number: 1 } }).exec();
+  }
+
+  async get(id: string): Promise<KindergartenProps> {
+    return await this.model.findById(id).exec();
+  }
+
+  async create(
+    createKindergartenDTO: CreateKindergartenInput,
+  ): Promise<KindergartenProps> {
+    const createdKeyCode = new this.model(createKindergartenDTO);
+
+    return await createdKeyCode.save();
+  }
+
+  async removeKindergarten(kindergartenId: string): Promise<void> {
+    this.model.findByIdAndDelete(kindergartenId).exec();
+  }
+
+  // for e2e purpose only
+  async clearTable(): Promise<void> {
+    await this.model.deleteMany({});
+  }
+}
