0bba6a444789bfa8ac16f324fb651cd385698cc1
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-08-09 21:33:18 +0200

    replace with apollo/client

--- client/src/apollo_client.ts
+++ client/src/apollo_client.ts
@@ -1,6 +1,10 @@
-import ApolloClient from 'apollo-boost';
+import { ApolloClient, ApolloLink, concat, InMemoryCache, HttpLink } from '@apollo/client';
 
-export const client = new ApolloClient({
+const link = new HttpLink({
     uri: process.env.REACT_APP_GRAPHQL_SERVER,
     credentials: 'include',
+});
+export const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: concat(authMiddleware, link),
 });
