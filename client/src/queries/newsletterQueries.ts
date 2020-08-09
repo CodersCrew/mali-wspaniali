a3e3f193e97059c337327e0d7a3a7667f5840516
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-08-10 00:40:48 +0200

    refactor queries

--- client/src/queries/newsletterQueries.ts
+++
@@ -1,6 +0,0 @@
-import { firebase } from '../firebase/firebase';
-import { Newsletter } from '../pages/Newsletter/types';
-
-export const createNewsletter = async (newsletterData: Newsletter) => {
-    return firebase.newsletter.saveNewsletter(newsletterData);
-};
--- client/src/queries/notificationQueries.ts
+++
@@ -1,5 +0,0 @@
-import { firebase } from '../firebase/firebase';
-
-export const setNotificationReadValue = (userId: string, notificationId: string, value: boolean) => {
-    return firebase.notification.setNotificationReadValue(userId, notificationId, value);
-};
