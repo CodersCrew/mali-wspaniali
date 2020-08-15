32272b7fdc94b870f9f279e2057c6c46da80f9d5
KristinaVolk <kristinavolk@coderscrew.pl>
2020-08-15 21:02:49 +0200

    contentType [implemented]

--- client/src/components/Tabs.tsx
+++ client/src/components/Tabs.tsx
@@ -2,17 +2,22 @@ import React, { FC } from 'react';
 import { Tabs as MuiTabs, makeStyles, createStyles, Theme, TabsProps } from '@material-ui/core/';
 import { Tab } from './Tab';
 
+type contentType = {
+    value: string;
+    label: string;
+};
+
 export const Tabs: FC<TabsProps> = ({ children, ...props }) => {
     const classes = useStyles();
-    let content: Array<{}> | null;
+    let content: contentType[] | null;
 
     if (children) {
-        content = children as Array<{}>;
+        content = children as contentPropsType[];
     }
 
     return (
         <MuiTabs classes={{ flexContainer: classes.flexContainer, indicator: classes.indicator }} {...props}>
-            {content!.map((contentItem: any) => (
+            {content!.map((contentItem: contentType) => (
                 <Tab key={contentItem.label} value={contentItem.value} label={contentItem.label} />
             ))}
         </MuiTabs>
