0e7a735c7312aefc32fc5eba3705173b507f732c
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-08-18 00:37:34 +0200

    rename filenames

--- server/src/agreements/domain/queries/handlers/get_all_agreements_handler.ts
+++ server/src/agreements/domain/queries/handlers/get_all_agreements_handler.ts
@@ -2,7 +2,7 @@ import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 
 import { AgreementRepository } from '../../repositories/agreement_repository';
 import { GetAllAgreementsQuery } from '../impl/get_all_agreements_query';
-import { AgreementProps } from '../../../../agreements/schemas/agreement_schema';
+import { AgreementProps } from '../../../schemas/agreement_schema';
 
 @QueryHandler(GetAllAgreementsQuery)
 export class GetAllAgreementsHandler
