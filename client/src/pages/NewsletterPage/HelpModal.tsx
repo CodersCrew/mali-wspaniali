821a165fc8f8c2c81ad5b6c2792c13138e7bfebe
Krzysiek Karczewski <krzysztofkarczewski92@gmail.com>
2020-08-25 21:20:27 +0200

    fix Icon position in HelpModal on NewsletterPage

--- client/src/pages/NewsletterPage/HelpModal.tsx
+++ client/src/pages/NewsletterPage/HelpModal.tsx
@@ -29,7 +29,7 @@ const useStyles = makeStyles(() =>
             width: 24,
             height: 24,
             position: 'absolute',
-            top: 75,
+            top: 45,
             left: 40,
         },
         modalContent: {
