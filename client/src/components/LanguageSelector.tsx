6b272434eef861b0e1718e739dc0a21649b5538f
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-08-30 17:18:29 +0200

    adjust flag css

--- client/src/components/LanguageSelector.tsx
+++ client/src/components/LanguageSelector.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IconButton } from '@material-ui/core';
+import { IconButton, createStyles, makeStyles, Theme } from '@material-ui/core';
 
 import PlFlag from '../assets/pl.png';
 import EnFlag from '../assets/en.png';
@@ -10,13 +10,28 @@ interface Props {
 }
 
 export const LanguageSelector = ({ language, onClick }: Props) => {
+    const classes = useStyles();
+
     const languageImage = (flag: string, lang: string) => {
         return (
-            <IconButton aria-label="change language" onClick={() => onClick(lang)}>
-                <img src={flag} alt={lang} />
+            <IconButton
+                aria-label="change language"
+                onClick={() => onClick(lang)}
+            >
+                <img src={flag} alt={lang} className={classes.flag} />
             </IconButton>
         );
     };
 
     return <div>{language === 'pl' ? languageImage(EnFlag, 'en') : languageImage(PlFlag, 'pl')}</div>;
 };
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        flag: {
+            borderRadius: '50%',
+            width: 18,
+            height: 18,
+        },
+    }),
+);
