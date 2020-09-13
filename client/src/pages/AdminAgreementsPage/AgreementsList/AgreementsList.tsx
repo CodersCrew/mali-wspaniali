6fdb711c8df25955d21a88ac32523ad9a57da828
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-09-13 11:50:43 +0200

    add Agreement list

---
+++ client/src/pages/AdminAgreementsPage/AgreementsList/AgreementsList.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { TableContainer, Paper, Table, TableHead, TableBody, makeStyles, createStyles, Theme } from '@material-ui/core';
+import { ChildrenFromKindergartenList } from './ChildrenFromKindergartenList';
+import { Kindergarten } from '../../../graphql/types';
+import { SortableHeader } from './SortableHeader';
+
+interface Props {
+    kindergartens: Kindergarten[];
+}
+
+export function AgreementsList({ kindergartens }: Props) {
+    const classes = useStyles();
+
+    return (
+        <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                    <SortableHeader />
+                </TableHead>
+                <TableBody>
+                    {kindergartens.map(kindergarten => (
+                        <ChildrenFromKindergartenList
+                            key={kindergarten._id}
+                            kindergarten={kindergarten}
+                            marketingAgreement={{ value: 35, total: 70 }}
+                            viewAgreement={{ value: 45, total: 45 }}
+                        />
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        table: {
+            minWidth: 650,
+        },
+    }),
+);
