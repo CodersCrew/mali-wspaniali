5fe8c2eece895561c3b1bb2851a566231c577c81
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-09-14 21:53:04 +0200

    add general status component

---
+++ client/src/components/Icons/Status.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import CancelIcon from '@material-ui/icons/Cancel';
+import { createStyles, makeStyles, Theme } from '@material-ui/core';
+
+export function Status({ success }: { success?: boolean }) {
+    const classes = useStyles();
+
+    return (
+        <span>
+            {success ? (
+                <CheckCircleIcon classes={{ root: classes.successStatus }} />
+            ) : (
+                <CancelIcon classes={{ root: classes.notSuccessStatus }} />
+            )}
+        </span>
+    );
+}
+
+const useStyles = makeStyles((ttheme: Theme) =>
+    createStyles({
+        successStatus: {
+            color: ttheme.palette.success.dark,
+            width: 13,
+            height: 13,
+        },
+        notSuccessStatus: {
+            color: ttheme.palette.error.dark,
+            width: 13,
+            height: 13,
+        },
+    }),
+);
