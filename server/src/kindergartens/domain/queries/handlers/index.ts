c3d589e1c5db3c7fa448661bc98ac58b67ef2b33
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-09-16 02:14:56 +0200

    add kindergarten with users query

--- server/src/kindergartens/domain/queries/handlers/index.ts
+++ server/src/kindergartens/domain/queries/handlers/index.ts
@@ -1,3 +1,7 @@
 import { GetAllKindergartensHandler } from './get_all_kindergartens_handler';
+import { GetKindergartenWithUsersHandler } from './get_kindergarten_with_users_handler';
 
-export const QueryHandlers = [GetAllKindergartensHandler];
+export const QueryHandlers = [
+  GetAllKindergartensHandler,
+  GetKindergartenWithUsersHandler,
+];
