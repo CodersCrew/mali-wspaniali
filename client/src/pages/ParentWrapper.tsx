ec993c51ee9fec8d9f7b62ade3467908ccdee216
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-09-24 22:39:28 +0200

    populate AddChildModal with kindergartens, children

--- client/src/pages/ParentWrapper.tsx
+++ client/src/pages/ParentWrapper.tsx
@@ -1,11 +1,22 @@
 import React, { FC, useContext } from 'react';
+import { useQuery } from '@apollo/client';
+
 import { AddChildModal } from '../components/AddChildModal/AddChildModal';
+import { UserContext } from './AppWrapper';
+import { KindergartenResponse, KINDERGARTENS } from '../graphql/kindergartensRepository';
 
 export const ParentWrapper: FC = ({ children }) => {
+    const user = useContext(UserContext);
+    const { data: kindergartenData } = useQuery<KindergartenResponse>(KINDERGARTENS);
+
+    if (!user || !kindergartenData) return null;
+
     return (
         <>
             <AddChildModal
                 handleSubmit={data => console.log(data)}
+                isOpen={user.children.length === 0}
+                kindergartens={kindergartenData.kindergartens}
             />
             {children}
         </>
