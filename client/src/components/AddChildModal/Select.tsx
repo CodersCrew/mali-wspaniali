abd8c3e4f45101e40e9430f9aaf61b5fb5e57560
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-09-26 16:17:58 +0200

    adjust Select

--- client/src/components/AddChildModal/Select.tsx
+++ client/src/components/AddChildModal/Select.tsx
@@ -1,74 +1,73 @@
-import React, { ChangeEvent, ReactNode } from 'react';
-import { InputLabel, FormControl, Select as SelectField } from '@material-ui/core';
-import MenuItem from '@material-ui/core/MenuItem';
+import React from 'react';
+import {
+    InputLabel,
+    FormControl,
+    Select as SelectField,
+    MenuItem,
+    FormHelperText,
+    makeStyles,
+    createStyles,
+} from '@material-ui/core';
+import { Theme } from '../../theme/types';
 
-import FormHelperText from '@material-ui/core/FormHelperText';
-import { useTranslation } from 'react-i18next';
-import { useStyles } from './useStyles';
-import { selectValues, Option } from './selectValues';
+import { Option } from './selectValues';
 
-interface SelectProps {
+interface Props {
     label: string;
     value: string;
+    options: Option[];
     name: string;
     disabled?: boolean;
-    className: string;
     error: string | undefined;
     touched: any;
-    onChange: (event: ChangeEvent<{ name?: string | undefined; value: unknown }>, child: ReactNode) => void;
+    onChange: (name: string, value: string) => void;
 }
 
-export const Select = ({ label, value, className, disabled, name, error, touched, onChange }: SelectProps) => {
-    const { t } = useTranslation();
+export function Select({ label, value, options, disabled, name, error, touched, onChange }: Props) {
+    const classes = useStyles();
     const errorText = error && touched ? error : '';
 
     return (
-        <FormControl variant="outlined" className={className}>
+        <FormControl variant="outlined" fullWidth className={classes.container}>
             <InputLabel htmlFor={name} error={!!errorText}>
                 {label}
             </InputLabel>
             <SelectField
                 error={!!errorText}
-                id="outlined-select-currency"
+                id={name}
                 label={label}
                 value={value}
                 name={name}
-                renderValue={() => value}
+                renderValue={() => options.find(o => o.value === value)?.label}
                 labelId={name}
                 disabled={!!disabled}
-                onChange={onChange}
+                fullWidth
+                onChange={({ target: { name: selectName, value: selectValue } }) => {
+                    onChange(selectName as string, selectValue as string);
+                }}
             >
-                {selectValues[name].map((option: Option, i: number) => {
-                    const path = `add-child-modal.select-options.${name}.option-label-${i + 1}`;
-                    const optionLabel = t(path) !== path ? t(path) : option.label;
-                    const helperLabelPath = `add-child-modal.select-options.${name}.helper-label-${i + 1}`;
-                    const helperLabel =
-                        t(helperLabelPath) !== helperLabelPath && option.helperLabel ? t(helperLabelPath) : '';
-
+                {options.map((option: Option) => {
                     return (
-                        <MenuItem value={optionLabel} key={optionLabel}>
-                            <OptionField info={helperLabel} label={optionLabel} />
+                        <MenuItem value={option.value} key={option.value}>
+                            <span>{option.label}</span>
+                            <span className={classes.helperLabel}>{option.helperLabel}</span>
                         </MenuItem>
                     );
                 })}
             </SelectField>
-            <FormHelperText error={!!errorText}>{errorText}</FormHelperText>
+            <FormHelperText error>{errorText}</FormHelperText>
         </FormControl>
     );
-};
-
-interface OptionProps {
-    info: string;
-    label: string;
 }
 
-function OptionField(props: OptionProps) {
-    const classes = useStyles();
-
-    return (
-        <>
-            <span>{props.label}</span>
-            <span className={classes.helperLabel}>{props.info}</span>
-        </>
-    );
-}
+export const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        helperLabel: {
+            color: theme.palette.grey['400'],
+            marginLeft: theme.spacing(1),
+        },
+        container: {
+            minWidth: 120,
+        },
+    }),
+);
