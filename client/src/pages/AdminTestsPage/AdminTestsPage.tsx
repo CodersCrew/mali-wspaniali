6a759e0898d9511a733835258cd8c07398ad90a0
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-10-04 02:56:52 +0200

    add add test button

--- client/src/pages/AdminTestsPage/AdminTestsPage.tsx
+++ client/src/pages/AdminTestsPage/AdminTestsPage.tsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from 'react';
 import { createStyles, Grid, makeStyles, Theme } from '@material-ui/core';
+import { useTranslation } from 'react-i18next';
 
 import { activePage } from '../../apollo_client';
 import { TestHistoryList } from './TestHistoryList/TestHistoryList';
+import { ButtonSecondary } from '../../components/Button';
 
 export function TestManagementPage() {
     const classes = useStyles();
+    const { t } = useTranslation();
 
     useEffect(() => {
         activePage(['admin-menu.test-management']);
@@ -14,8 +17,15 @@ export function TestManagementPage() {
     return (
         <Grid container>
             <Grid item sm={12}>
-                <div className={classes.listContainer}>
-                    <TestHistoryList />
+                <div className={classes.container}>
+                    <div className={classes.searchContainer}>
+                        <ButtonSecondary variant="contained">
+                            {t('manage-test-view.test-search.create-test')}
+                        </ButtonSecondary>
+                    </div>
+                    <div className={classes.listContainer}>
+                        <TestHistoryList />
+                    </div>
                 </div>
             </Grid>
         </Grid>
@@ -24,8 +34,14 @@ export function TestManagementPage() {
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
+        container: {
+            margin: `0 ${theme.spacing(3)}px`,
+        },
+        searchContainer: {
+            marginTop: 44,
+        },
         listContainer: {
-            margin: `50px ${theme.spacing(3)}px 0`,
+            marginTop: 50,
         },
     }),
 );
