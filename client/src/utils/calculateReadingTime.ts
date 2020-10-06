8c0eac24f31e84daa74f0a367132e35c6a7423ec
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-10-06 23:38:23 +0200

    adjust types

--- client/src/utils/calculateReadingTime.ts
+++ client/src/utils/calculateReadingTime.ts
@@ -1,10 +1,7 @@
-/**
- * Reading time is calculate based on library: https://github.com/ngryman/reading-time/blob/master/lib/reading-time.js
- * @param articleContent
- */
-export const calculateReadingTime = (articleContent: string): number => {
-    const amountWordsInArticles: number = articleContent.split(' ').length;
-    const readingTimeInMinutes: number = amountWordsInArticles / 200;
+// https://github.com/ngryman/reading-time/blob/master/lib/reading-time.js
+export function calculateReadingTime(articleContent: string): number {
+    const amountWordsInArticles = articleContent.split(' ').length;
+    const readingTimeInMinutes = amountWordsInArticles / 200;
 
     return Math.ceil(Number(readingTimeInMinutes.toFixed(2)));
-};
+}
