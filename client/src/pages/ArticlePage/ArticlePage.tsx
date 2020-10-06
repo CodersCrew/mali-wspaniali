ba490c37c7da223e05d4b55f0ef54a9b8f2756f3
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-10-07 01:24:06 +0200

    adjust ArticlePage

--- client/src/pages/ArticlePage/ArticlePage.tsx
+++ client/src/pages/ArticlePage/ArticlePage.tsx
@@ -1,23 +1,26 @@
 import React, { useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import { makeStyles, createStyles, Grid, Theme } from '@material-ui/core';
+import { makeStyles, createStyles, Grid, Theme, CardMedia, Divider } from '@material-ui/core';
 import { useQuery } from '@apollo/client';
-import { ArticleContent, customArticleTheme } from './ArticleContent';
+import clsx from 'clsx';
+import { ArticleContent } from './ArticleContent';
 import { ArticleVideo } from './ArticleVideo';
 import { ArticleRedactor } from './ArticleRedactor';
 import { ArticleNavigationMobile } from '../ArticleListPage/ArticleNavigationMobile';
 import { Article } from '../../graphql/types';
 import { ARTICLE_BY_ID } from '../../graphql/articleRepository';
 import { activePage } from '../../apollo_client';
-import { useBreakpoints } from '../../queries/useBreakpoints';
+import { useIsDevice } from '../../queries/useBreakpoints';
 import { ButtonDefault } from '../../components/Button';
+import { ReadingTime } from './ReadingTime';
+import { TagList } from './TagList';
 
 export const ArticlePage = () => {
-    const classes = useStyles();
     const { t } = useTranslation();
     const { articleId } = useParams<{ articleId: string }>();
-    const device = useBreakpoints();
+    const { isMobile, isTablet, isDesktop } = useIsDevice();
+    const classes = useStyles();
     const history = useHistory();
     const { data } = useQuery<{ article: Article }>(ARTICLE_BY_ID, {
         variables: { articleId },
@@ -32,88 +35,89 @@ export const ArticlePage = () => {
     }
 
     if (!data || !data.article) return null;
-    const { pictureUrl, date, readingTime, title, description, contentHTML } = data.article;
+
+    const { pictureUrl, date, readingTime, title, description, contentHTML, tags, videoUrl, redactor } = data.article;
 
     return (
-        <React.Fragment>
-            {device === 'MOBILE' && <ArticleNavigationMobile onClick={onBackClick} />}
-            <Grid className={classes.rootGrid} container direction="column">
-                <Grid className={classes.articleContentContainer} item xs={12} lg={10}>
-                    <ArticleContent
-                        pictureUrl={pictureUrl}
-                        date={date}
-                        readingTime={readingTime}
-                        title={title}
-                        description={description}
-                        contentHTML={contentHTML}
-                    />
-
-                    <Grid className={classes.videoContainer}>
-                        <ArticleVideo videoUrl={data.article.videoUrl} tags={data.article.tags} />
+        <>
+            {isMobile && <ArticleNavigationMobile onClick={onBackClick} />}
+            <Grid
+                classes={{ root: clsx({ [classes.container]: true, [classes.mobileContainer]: !isDesktop }) }}
+                container
+                direction="column"
+            >
+                <CardMedia classes={{ root: classes.imageContainer }} component="img" image={pictureUrl} />
+                <Grid item xs={12} lg={10} md={11}>
+                    <div
+                        className={clsx({
+                            [classes.readingTimeContainerMobile]: true,
+                            [classes.readingTimeContainerTablet]: isTablet,
+                            [classes.readingTimeContainerDesktop]: isDesktop,
+                        })}
+                    >
+                        <ReadingTime date={new Date(date)} readingTime={readingTime} />
+                    </div>
+                    <ArticleContent title={title} description={description} contentHTML={contentHTML} />
+                    <Grid item classes={{ root: classes.videoContainer }}>
+                        <ArticleVideo videoUrl={videoUrl} />
                     </Grid>
-                    <Grid container direction="row" className={classes.mainRedactorWrapper}>
-                        <ArticleRedactor redactor={data.article.redactor} />
+                    <Grid item classes={{ root: classes.tagContainer }}>
+                        <TagList tags={tags} />
                     </Grid>
                 </Grid>
-                <Grid className={classes.paginationButtonsContainer}>
-                    <ButtonDefault variant="contained">{t(`single-article.go-to-previous-page`)}</ButtonDefault>
-                    <ButtonDefault variant="contained" color="secondary">
-                        {t(`single-article.go-to-next-page`)}
-                    </ButtonDefault>
+                <Grid container xs={12}>
+                    <Grid item xs={12}>
+                        <Divider />
+                    </Grid>
+                    <Grid item xs={12} classes={{ root: classes.redactorContainer }}>
+                        <ArticleRedactor redactor={redactor} />
+                    </Grid>
+                </Grid>
+                <Grid container xs={12} justify="space-between">
+                    <Grid item>
+                        <ButtonDefault variant="contained">{t(`single-article.go-to-previous-page`)}</ButtonDefault>
+                    </Grid>
+                    <Grid item>
+                        <ButtonDefault variant="contained" color="secondary">
+                            {t(`single-article.go-to-next-page`)}
+                        </ButtonDefault>
+                    </Grid>
                 </Grid>
             </Grid>
-        </React.Fragment>
+        </>
     );
 };
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
-        rootGrid: {
-            padding: 0,
-
-            [theme.breakpoints.down('sm')]: {
-                overflow: 'hidden',
-            },
-        },
-        articleContentContainer: {
-            position: 'relative',
+        container: {
             display: 'flex',
-            flexDirection: 'column',
             alignItems: 'center',
             padding: theme.spacing(3),
-
-            [theme.breakpoints.down('lg')]: {
-                minWidth: '96vw',
-            },
-            [theme.breakpoints.up('lg')]: {
-                minWidth: 0,
-                padding: theme.spacing(4),
-            },
-            [customArticleTheme.breakpoints.down('xs')]: {
-                padding: theme.spacing(2),
-            },
+        },
+        mobileContainer: {
+            padding: theme.spacing(2),
+        },
+        imageContainer: {
+            height: 400,
+        },
+        readingTimeContainerMobile: {
+            margin: theme.spacing(3, 0, 2),
+        },
+        readingTimeContainerTablet: {
+            margin: theme.spacing(4, 0, 2),
+        },
+        readingTimeContainerDesktop: {
+            margin: theme.spacing(5, 0, 2),
         },
         videoContainer: {
-            paddingBottom: theme.spacing(4),
-
-            [theme.breakpoints.up('xl')]: {
-                paddingBottom: theme.spacing(5),
-            },
+            margin: `${theme.spacing(4)}px 0`,
         },
-        mainRedactorWrapper: {
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+        tagContainer: {
+            marginBottom: theme.spacing(4),
         },
-        paginationButtonsContainer: {
-            display: 'flex',
-            justifyContent: 'space-between',
-            width: '100%',
-            padding: theme.spacing(1, 3, 3, 3),
-
-            [theme.breakpoints.up('xl')]: {
-                paddingTop: theme.spacing(2),
-            },
+        redactorContainer: {
+            margin: `${theme.spacing(4)}px 0`,
         },
     }),
 );
