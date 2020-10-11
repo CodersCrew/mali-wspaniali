11b0ef842fc8ed8feb0b7ec44b408f32af92ec18
Piotr Pietruszka <piotr.pietruszka@o2.pl>
2020-10-11 17:43:17 +0200

    Revert "Malwsp 391 adjust child page"

--- client/src/pages/ChildProfile/Details/ChildDetails.tsx
+++ client/src/pages/ChildProfile/Details/ChildDetails.tsx
@@ -1,44 +1,5 @@
-import React, { useContext } from 'react';
-import { useQuery } from '@apollo/client';
-import { createStyles, makeStyles, Typography } from '@material-ui/core';
-import { useTranslation } from 'react-i18next';
-import { EditChildModal } from './EditChildModal';
-import { UserContext } from '../../AppWrapper';
-import { Theme } from '../../../theme';
-import { KindergartenResponse, KINDERGARTENS } from '../../../graphql/kindergartensRepository';
+import React from 'react';
 
 export function ChildDetails() {
-    const user = useContext(UserContext);
-    const { data: kindergartenData } = useQuery<KindergartenResponse>(KINDERGARTENS);
-    const classes = useStyles();
-    const { t } = useTranslation();
-
-    if (!user || !kindergartenData) return null;
-
-    return (
-        <div className={classes.mainContainer}>
-            <Typography variant="h4">{t('child-profile.child-details.form')} </Typography>
-            <EditChildModal
-                handleSubmit={_child => {
-                    /*UPDATE_CHILD({
-                        variables: {
-                            child: newChild,
-                        },
-                    });*/
-                }}
-                kindergartens={kindergartenData.kindergartens}
-            />
-        </div>
-    );
+    return <div>child details</div>;
 }
-
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        mainContainer: {
-            background: theme.palette.background.paper,
-            margin: theme.spacing(3),
-            padding: theme.spacing(2),
-            height: theme.spacing(72),
-        },
-    }),
-);
