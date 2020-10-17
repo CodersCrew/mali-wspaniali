804bf5ca94f85c89a6b56cf60a6ae44208a1ee49
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-10-17 15:09:30 +0200

    add cancel button, validate form on each change

--- client/src/pages/AdminAddTestPage/AdminAddTestPage.tsx
+++ client/src/pages/AdminAddTestPage/AdminAddTestPage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { createStyles, Grid, makeStyles, Theme } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router-dom';
 
 import { activePage } from '../../apollo_client';
 import { BasicInformationForm } from './BasicInformationForm';
@@ -8,41 +9,106 @@ import { KindergartenPicker } from './KindergartenPicker';
 import { TestInformation } from './TestInformation';
 import { useAddTest } from './useAddTest';
 import { openSnackbar } from '../../components/Snackbar/openSnackbar';
+import { ButtonSecondary } from '../../components/Button';
+import { ArrowTooltip } from '../../components/Tooltip/ArrowTooltip';
 
 export function AdminAddTestPage() {
     const classes = useStyles();
     const { t } = useTranslation();
-    const { testInformation, setTestInformation, submit, kindergartens, selectKindergarten } = useAddTest(result => {
+    const history = useHistory();
+    const {
+        testInformation,
+        setTestInformation,
+        submit,
+        kindergartens,
+        selectKindergarten,
+        reasonForBeingDisabled,
+    } = useAddTest(result => {
         if ('errors' in result) {
             openSnackbar({ text: t(result.errors), severity: 'error' });
         } else {
             openSnackbar({ text: t('add-test-view.assessment-created') });
         }
     });
+
+    function redirectIntoTestPage() {
+        history.push('/admin/test-management');
+    }
+
     useEffect(() => {
         activePage(['admin-menu.test-management']);
     }, []);
 
     return (
-        <Grid container className={classes.container} spacing={2}>
-            <Grid item sm={7}>
-                <Grid container direction="column" spacing={2}>
-                    <Grid item sm={12}>
-                        <BasicInformationForm value={testInformation} onChange={setTestInformation} />
+        <div className={classes.container}>
+            <Grid container direction="column" alignItems="flex-end" spacing={6}>
+                <Grid item>
+                    <Grid container spacing={2}>
+                        <Grid item xs={7}>
+                            <Grid container direction="column" spacing={2}>
+                                <Grid item sm={12}>
+                                    <BasicInformationForm value={testInformation} onChange={setTestInformation} />
+                                </Grid>
+                                <Grid item sm={12}>
+                                    <TestInformation />
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={5}>
+                            <KindergartenPicker
+                                kindergartens={kindergartens}
+                                onSelect={value => selectKindergarten(value)}
+                            />
+                        </Grid>
                     </Grid>
-                    <Grid item sm={12}>
-                        <TestInformation />
+                </Grid>
+                <Grid item>
+                    <Grid container spacing={2}>
+                        <Grid item>
+                            <ButtonSecondary variant="text" data-testid="create-button" onClick={redirectIntoTestPage}>
+                                {t('add-test-view.cancel')}
+                            </ButtonSecondary>
+                        </Grid>
+                        <Grid item>
+                            <SubmitButton onClick={submit} reasonForBeingDisabled={reasonForBeingDisabled} />
+                        </Grid>
                     </Grid>
                 </Grid>
             </Grid>
-            <Grid item sm={5}>
-                <KindergartenPicker kindergartens={kindergartens} onSelect={value => selectKindergarten(value)} />
-            </Grid>
-            <button data-testid="create-button" onClick={() => submit()}>
-                Create a new test
-            </button>
-        </Grid>
+        </div>
     );
+}
+
+interface SubmitButtonProps {
+    onClick: () => void;
+    reasonForBeingDisabled?: string;
+}
+
+function SubmitButton({ onClick, reasonForBeingDisabled }: SubmitButtonProps) {
+    const { t } = useTranslation();
+
+    const button = (
+        <ButtonSecondary
+            variant="contained"
+            data-testid="create-button"
+            onClick={onClick}
+            disabled={!!reasonForBeingDisabled}
+        >
+            {t('add-test-view.create-test')}
+        </ButtonSecondary>
+    );
+
+    if (reasonForBeingDisabled) {
+        const translatedDisableReason = t(reasonForBeingDisabled);
+
+        return (
+            <ArrowTooltip title={translatedDisableReason}>
+                <span>{button}</span>
+            </ArrowTooltip>
+        );
+    }
+
+    return button;
 }
 
 const useStyles = makeStyles((theme: Theme) =>
