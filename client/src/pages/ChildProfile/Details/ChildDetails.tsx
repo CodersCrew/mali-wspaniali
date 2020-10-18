12bbdcfef519f38957f13ccbf2bd16fa614f5a0f
VolkCristina <kvolk@dynak.com>
2020-10-18 18:36:32 +0100

    conflicts resolving with error

--- client/src/pages/ChildProfile/Details/ChildDetails.tsx
+++ client/src/pages/ChildProfile/Details/ChildDetails.tsx
@@ -1,38 +1,34 @@
-import React from 'react';
 import React, { useContext } from 'react';
+import { useQuery } from '@apollo/client';
 import { createStyles, makeStyles, Typography } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
 import { EditChildModal } from './EditChildModal';
 import { UserContext } from '../../AppWrapper';
 import { Theme } from '../../../theme';
-import { KindergartenResponse, KINDERGARTENS } from '../../../graphql/kindergartensRepository';
+import { KindergartenResponse, KINDERGARTEN_WITH_USERS } from '../../../graphql/kindergartensRepository';
 
 export function ChildDetails() {
-    return <div>child details</div>;
     const user = useContext(UserContext);
-    const { data: kindergartenData } = useQuery<KindergartenResponse>(KINDERGARTENS);
+    const { data: kindergartenData } = useQuery<KindergartenResponse>(KINDERGARTEN_WITH_USERS);
     const classes = useStyles();
     const { t } = useTranslation();
-    );
-        </div>
-            />
-                kindergartens={kindergartenData.kindergartens}
-                }}
-                    });*/
-                        },
-                            child: newChild,
-                        variables: {
-                    /*UPDATE_CHILD({
-                handleSubmit={_child => {
-            <EditChildModal
-            <Typography variant="h4">{t('child-profile.child-details.form')} </Typography>
-        <div className={classes.mainContainer}>
-    return (
-
     if (!user || !kindergartenData) return null;
-
+    return (
+        <div className={classes.mainContainer}>
+            <Typography variant="h4">{t('child-profile.child-details.form')} </Typography>
+            <EditChildModal
+                handleSubmit={_child => {
+                    /*UPDATE_CHILD({
+                        variables: {
+                            child: newChild,
+                        },
+                    });*/
+                }}
+                kindergartens={kindergartenData.kindergartens}
+            />
+        </div>
+    );
 }
-
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         mainContainer: {
