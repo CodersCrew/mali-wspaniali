551f7194e181d7fbf1470f9602ebd6cd0e2192c4
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-10-24 20:57:09 +0200

    makes translationOf more generic
    Now translationOf handles the variables change

--- client/src/utils/testing/isTranslationOf.ts
+++ client/src/utils/testing/isTranslationOf.ts
@@ -2,10 +2,14 @@ import i18n from 'i18next';
 
 import '../../internationalization/i18n';
 
-export function isTranslationOf(localization: string, value: string): boolean {
-    return translationOf(localization) === value;
+export function isTranslationOf(
+    localization: string,
+    value: string,
+    options?: { [key: string]: string | number },
+): boolean {
+    return translationOf(localization, options) === value;
 }
 
-export function translationOf(localization: string): string {
-    return i18n.t(localization);
+export function translationOf(localization: string, options?: { [key: string]: string | number }): string {
+    return i18n.t(localization, options);
 }
