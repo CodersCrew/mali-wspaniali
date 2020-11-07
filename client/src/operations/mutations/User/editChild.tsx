ecf7bf9120f525f9552599abe3bde8c91d198928
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-11-07 16:49:05 +0100

    refactor

--- client/src/operations/mutations/User/editChild.tsx
+++ client/src/operations/mutations/User/editChild.tsx
@@ -1,5 +1,5 @@
 import { gql, useMutation } from '@apollo/client';
-import { UpdatedChildInput } from '../../../graphql/types';
+import { Child, UpdatedChildInput, Kindergarten } from '../../../graphql/types';
 import { client } from '../../../apollo_client';
 import { GET_ME, useGetMe } from './useGetMe';
 import { useKindergartens } from '../../queries/Kindergartens/getKindergartens';
@@ -23,6 +23,12 @@ export function useEditChild() {
 
     return {
         editChild: (update: UpdatedChildInput) => {
+    const updatedKindergarten =kindergartenList?.find(k => k._id === update.kindergartenId); 
+
+    if (!updatedKindergarten) return;
+
+
+
             editChild({
                 variables: {
                     child: update,
@@ -35,25 +41,21 @@ export function useEditChild() {
                     data: {
                         me: {
                             ...user,
-                            children: user.children.map(c => {
-                                const { childId, kindergartenId, ...child } = update;
-
-                                if (c._id === childId) {
-                                    const newKindergarten = kindergartenList.find(k => k._id === kindergartenId);
-
-                                    if (newKindergarten) {
-                                        return { ...c, ...child, kindergarten: newKindergarten };
-                                    }
-
-                                    return { ...c, ...child };
-                                }
-
-                                return c;
-                            }),
+                            children: user.children.map(c => updateEditedChild(c, updatedKindergarten, update)),
                         },
                     },
                 });
             });
         },
     };
+}
+
+function updateEditedChild(cachedChild: Child, kindergarten: Kindergarten, update: UpdatedChildInput) {
+    const { childId, kindergartenId, ...updatedChild } = update;
+
+    if (cachedChild._id === childId) {
+            return { ...cachedChild, ...updatedChild, kindergarten };
+    }
+
+    return cachedChild
 }
