bef0415f67217190d8cfb1790ff672305a9a259f
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-11-07 15:57:51 +0100

    update child cache

--- client/src/operations/mutations/User/editChild.tsx
+++ client/src/operations/mutations/User/editChild.tsx
@@ -1,5 +1,9 @@
 import { gql, useMutation } from '@apollo/client';
 import { UpdatedChildInput } from '../../../graphql/types';
+import { client } from '../../../apollo_client';
+import { GET_ME } from '../../../graphql/userRepository';
+import { useGetMe } from './useGetMe';
+import { useKindergartens } from '../../queries/Kindergartens/getKindergartens';
 
 export interface EditChildResponse {
     status: boolean;
@@ -15,6 +19,8 @@ const EDIT_CHILD = gql`
 
 export function useEditChild() {
     const [editChild] = useMutation<EditChildResponse, { child: UpdatedChildInput }>(EDIT_CHILD);
+    const { user } = useGetMe();
+    const { kindergartenList } = useKindergartens();
 
     return {
         editChild: (update: UpdatedChildInput) => {
@@ -22,6 +28,32 @@ export function useEditChild() {
                 variables: {
                     child: update,
                 },
+            }).then(() => {
+                if (!user || !kindergartenList) return;
+
+                client.writeQuery({
+                    query: GET_ME,
+                    data: {
+                        me: {
+                            ...user,
+                            children: user.children.map(c => {
+                                const { childId, kindergartenId, ...child } = update;
+
+                                if (c._id === childId) {
+                                    const newKindergarten = kindergartenList.find(k => k._id === kindergartenId);
+
+                                    if (newKindergarten) {
+                                        return { ...c, ...child, kindergarten: newKindergarten };
+                                    }
+
+                                    return { ...c, ...child };
+                                }
+
+                                return c;
+                            }),
+                        },
+                    },
+                });
             });
         },
     };
