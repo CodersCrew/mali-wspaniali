eab6e44b1d2620a89adf8955e52c41038a62d15f
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-11-23 22:26:14 +0100

    add client kindergarten created notification

--- client/src/pages/NotificationsPage/useNotificationContent.ts
+++ client/src/pages/NotificationsPage/useNotificationContent.ts
@@ -8,6 +8,7 @@ export function useNotificationContent(): { getNotification: (name: string, valu
         new_assessment: (values: string[]) => t('notification-list.new-assessment', { name: values[0] }),
         new_user: (values: string[]) => t('notification-list.new-user', { email: values[0] }),
         child_created: () => t('notification-list.child-created'),
+        kindergarten_created: (values: string[]) => t('notification-list.kindergarten-created', { name: values[0] }),
     };
 
     return {
