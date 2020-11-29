6f2781ab40138553f12ab3222b7ac6a47fcf6003
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-11-29 17:46:57 +0100

    add kindergarten with instructor value object

---
+++ server/src/assessment/domain/models/kindergarten_with_instructor_value_object.ts
@@ -0,0 +1,39 @@
+import { Props } from '../../../shared/domain/value_object';
+import { ValueObject } from '../../../shared/domain/value_object';
+import { Result } from '../../../shared/domain/result';
+import { Guard } from '../../../shared/utils/guard';
+import { ObjectId } from '../../../users/domain/models/object_id_value_object';
+
+type Value = Props<KindergartenWithInstructorProps>;
+
+interface KindergartenWithInstructorProps {
+  kindergartenId: ObjectId;
+  instructorId: ObjectId | null;
+}
+
+export class KindergartenWithInstructor extends ValueObject<
+  KindergartenWithInstructorProps
+> {
+  private constructor(props: Value) {
+    super(props);
+  }
+
+  public static create(
+    value: KindergartenWithInstructorProps,
+  ): Result<KindergartenWithInstructor> {
+    const isNull = Guard.againstNullOrUndefined(
+      value.kindergartenId,
+      'KindergartenId',
+    );
+
+    if (!isNull.succeeded) {
+      return Result.fail(isNull.message);
+    }
+
+    if (!(value.kindergartenId instanceof ObjectId)) {
+      return Result.fail('KindergartenWithInstructor must be valid');
+    }
+
+    return Result.ok(new KindergartenWithInstructor({ value }));
+  }
+}
