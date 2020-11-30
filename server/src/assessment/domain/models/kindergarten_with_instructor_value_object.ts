db9f594b5acd1aa0d082ae980a8f1bd3fdf4f3ff
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-11-30 13:42:19 +0100

    add kindergarten with instructor vo tests

--- server/src/assessment/domain/models/kindergarten_with_instructor_value_object.ts
+++ server/src/assessment/domain/models/kindergarten_with_instructor_value_object.ts
@@ -23,7 +23,7 @@ export class KindergartenWithInstructor extends ValueObject<
   ): Result<KindergartenWithInstructor> {
     const isNull = Guard.againstNullOrUndefined(
       value.kindergartenId,
-      'KindergartenId',
+      'kindergartenId',
     );
 
     if (!isNull.succeeded) {
