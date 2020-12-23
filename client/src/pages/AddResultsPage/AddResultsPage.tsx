042834bde3fc27d440301f066759ed8ad2a11642
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-12-23 16:09:50 +0100

    add fab

--- client/src/pages/AddResultsPage/AddResultsPage.tsx
+++ client/src/pages/AddResultsPage/AddResultsPage.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core';
+import { createStyles, Fab, makeStyles, Theme } from '@material-ui/core';
+import { BarChart } from '@material-ui/icons';
+import { useTranslation } from 'react-i18next';
 
 import { activePage } from '../../apollo_client';
 import { useAssessments } from '../../operations/queries/Assessment/getAllAssessments';
@@ -13,9 +15,12 @@ export function AddResultsPage() {
     const [selectedKindergarten, setSelectedKindergarten] = useState('');
     const [searchTerm, setSearchTerm] = useState('');
     const classes = useStyles();
+    const { t } = useTranslation();
 
-    const currentChildren = assessments.find(a => a._id === selectedAssessment)?.kindergartens
-                                        .find(k => k.kindergarten._id === selectedKindergarten)?.kindergarten.children || []
+    const currentChildren =
+        assessments
+            .find(a => a._id === selectedAssessment)
+            ?.kindergartens.find(k => k.kindergarten._id === selectedKindergarten)?.kindergarten.children || [];
 
     useEffect(() => {
         activePage(['instructor-menu.add-results']);
@@ -25,42 +30,52 @@ export function AddResultsPage() {
         const [assessment] = assessments;
 
         if (assessment) {
-            setSelectedAssessment(assessment._id)
+            setSelectedAssessment(assessment._id);
 
-            setSelectedKindergarten(assessment.kindergartens[0]?.kindergarten._id)
+            setSelectedKindergarten(assessment.kindergartens[0]?.kindergarten._id);
         }
-    }, [assessments])
+    }, [assessments]);
 
     return (
         <div className={classes.container}>
             <CustomContainer
-            header={
-                <ChildListHeader
-                    assessments={assessments}
-                    selectedAssessment={selectedAssessment}
-                    selectedKindergarten={selectedKindergarten}
-                    searchTerm={searchTerm}
-                    onChange={({ type, value }) => {
-                        if (type === 'assessment') {
-                            setSelectedAssessment(value);
+                header={
+                    <ChildListHeader
+                        assessments={assessments}
+                        selectedAssessment={selectedAssessment}
+                        selectedKindergarten={selectedKindergarten}
+                        searchTerm={searchTerm}
+                        onChange={({ type, value }) => {
+                            if (type === 'assessment') {
+                                setSelectedAssessment(value);
 
-                            return;
-                        }
+                                return;
+                            }
 
-                        if (type === 'searchTerm') {
-                            setSearchTerm(value)
+                            if (type === 'searchTerm') {
+                                setSearchTerm(value);
 
-                            return
-                        }
+                                return;
+                            }
 
-                        setSelectedKindergarten(value);
-                    }}
-                />
-            }
-            container={
-                <ChildListContainer searchTerm={searchTerm} childList={currentChildren} />
-            }
-        />
+                            setSelectedKindergarten(value);
+                        }}
+                    />
+                }
+                container={
+                    <ChildListContainer searchTerm={searchTerm} childList={currentChildren} />
+                }
+            />
+            <Fab
+                variant="extended"
+                color="secondary"
+                aria-label="add test result"
+                className={classes.fab}
+            >
+                <BarChart />
+                &nbsp;
+                {t('add-results-page.add-result')}
+            </Fab>
         </div>
     );
 }
@@ -68,7 +83,12 @@ export function AddResultsPage() {
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         container: {
-            padding: theme.spacing(3)
+            padding: theme.spacing(3),
+        },
+        fab: {
+            position: 'fixed',
+            bottom: theme.spacing(3),
+            right: theme.spacing(3),
         },
     }),
 );
