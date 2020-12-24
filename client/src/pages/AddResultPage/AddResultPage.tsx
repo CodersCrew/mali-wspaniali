dd6aa3983877060931e98a487f54e89ad6f94b4c
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-12-24 05:58:45 +0100

    add result creator page

---
+++ client/src/pages/AddResultPage/AddResultPage.tsx
@@ -0,0 +1,77 @@
+import React, { useEffect } from 'react';
+import { createStyles, Grid, makeStyles, Divider, Paper, Theme } from '@material-ui/core';
+import { useParams, useHistory } from 'react-router-dom';
+import { useAssessments } from '../../operations/queries/Assessment/getAllAssessments';
+import { ChildPicker } from './ChildPicker';
+import { ResultCreator } from './ResultCreator';
+import { activePage } from '../../apollo_client';
+
+export function AddResultPage() {
+    const { assessments } = useAssessments({ withChildren: true });
+    const params = useParams() as { [index: string]: string };
+    const history = useHistory();
+    const classes = useStyles();
+
+    useEffect(() => {
+        activePage(['instructor-menu.add-results']);
+    }, []);
+
+    const selectedAssessment = assessments.find(a => a._id === params.assessmentId);
+    const selectedKindergarten = selectedAssessment?.kindergartens.find(k => k.kindergarten._id === params.kindergartenId)?.kindergarten;
+    const selectedChild = selectedKindergarten?.children?.find(c => c._id === params.childId)
+
+    if (!selectedAssessment || !selectedKindergarten || !selectedChild) {
+        return null;
+    }
+
+    return (
+        <div className={classes.container}>
+            <Paper>
+                <Grid container>
+                    <Grid item xs={4}>
+                        <ChildPicker
+                            selectedKindergarten={selectedKindergarten._id || ''}
+                            kindergartens={selectedAssessment.kindergartens.map(k => k.kindergarten) || []}
+                            selected={selectedChild._id}
+                            childList={selectedKindergarten.children || []}
+                            onClick={(type, value) => {
+                                if (type === 'child') {
+                                    history.push(
+                                        `/instructor/result/add/${selectedAssessment._id}/${selectedKindergarten._id}/${value}`,
+                                    );
+                                }
+
+                                if (type === 'kindergarten') {
+
+                                    const currentSelectedKindergarten = selectedAssessment?.kindergartens.find(k => k.kindergarten._id === value)?.kindergarten;
+                                    const firstChildren = currentSelectedKindergarten?.children![0]
+
+                                    if (firstChildren) {
+                                        history.push(
+                                            `/instructor/result/add/${selectedAssessment._id}/${value}/${firstChildren._id}`,
+                                        ); 
+
+                                    }
+
+                                }
+                            }}
+                        />
+                    </Grid>
+                    <Grid item xs={8}>
+                        <ResultCreator selectedChild={selectedChild} />
+
+                        <Divider />
+                    </Grid>
+                </Grid>
+            </Paper>
+        </div>
+    );
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        container: {
+            padding: theme.spacing(3),
+        },
+    }),
+);
