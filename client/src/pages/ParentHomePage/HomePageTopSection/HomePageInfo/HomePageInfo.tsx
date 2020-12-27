185b5820e34306a093a31d3ca2384b096b982665
celinawierzbicka <celinawierzbicka@gmail.com>
2020-12-27 18:50:23 +0100

    Remove read more button

--- client/src/pages/ParentHomePage/HomePageTopSection/HomePageInfo/HomePageInfo.tsx
+++ client/src/pages/ParentHomePage/HomePageTopSection/HomePageInfo/HomePageInfo.tsx
@@ -1,22 +1,21 @@
-import React, { useState } from 'react';
-import { makeStyles, createStyles, Theme, Grid } from '@material-ui/core';
+import React from 'react';
+import { makeStyles, createStyles, Theme, Typography, Grid } from '@material-ui/core';
+import { Close } from '@material-ui/icons';
+import { useTranslation } from 'react-i18next';
+
 import { cardBackgroundColor } from '../../../../colors';
-import { HomePageInfoContent } from './HomePageInfoContent';
 import { HomePageInfoPropTypes } from './types';
 
 export const HomePageInfo = ({ toggleInfoComponent }: HomePageInfoPropTypes) => {
-    const [readMore, setReadMore] = useState(false);
     const classes = useStyles();
-
-    const toggleInfoText = () => setReadMore(!readMore);
+    const { t } = useTranslation();
 
     return (
-        <Grid className={`${classes.infoContainer} ${!readMore && classes.smallInfoContainer}`}>
-            <HomePageInfoContent
-                toggleInfoText={toggleInfoText}
-                toggleInfoComponent={toggleInfoComponent}
-                readMore={readMore}
-            />
+        <Grid className={classes.infoContainer}>
+            <Close className={classes.contentCloseIcon} onClick={() => toggleInfoComponent()} />
+            <Typography variant="h4" className={classes.contentTitle}>{t('home-page-content.foundation-header')}</Typography>
+            <Typography variant="body1" className={classes.contentDescription}>{t('home-page-content.foundation-content')}</Typography>
+            <Typography variant="body1">{t('home-page-content.foundation-content-summary')}</Typography>
         </Grid>
     );
 };
@@ -40,11 +39,31 @@ const useStyles = makeStyles((theme: Theme) =>
                 flexDirection: 'column',
                 overflowY: 'hidden',
             },
-
         },
-        smallInfoContainer: {
+        contentCloseIcon: {
+            width: 14,
+            height: 14,
+            position: 'absolute',
+            top: 14,
+            right: 14,
+            cursor: 'pointer',
+
             [theme.breakpoints.down('md')]: {
-                maxHeight: 200,
+                width: 20,
+                height: 20,
+                top: 10,
+                right: 10,
+            },
+        },
+        contentTitle: {
+            marginBottom: theme.spacing(2),
+        },
+        contentDescription: {
+            marginBottom: theme.spacing(2),
+
+            [theme.breakpoints.down('md')]: {
+                marginTop: 0,
+                height: 'auto',
             },
         },
     }),
--- client/src/pages/ParentHomePage/HomePageTopSection/HomePageInfo/types.ts
+++ client/src/pages/ParentHomePage/HomePageTopSection/HomePageInfo/types.ts
@@ -1,9 +1,3 @@
-export interface InfoContentPropTypes {
-    toggleInfoText(): void;
-    toggleInfoComponent(): void;
-    readMore: boolean;
-}
-
 export interface HomePageInfoPropTypes {
     toggleInfoComponent(): void;
 }
