ab5ca02840448b97deed4f4577c9ddf7ec4be310
Tom-Bul <t.bulat99@gmail.com>
2021-01-12 20:53:02 +0100

    test

--- client/src/components/Blog/BlogArticleCard.tsx
+++ client/src/components/Blog/BlogArticleCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardMedia, CardContent, CardActions, Typography, makeStyles, Theme } from '@material-ui/core';
+import { Card,  CardMedia, CardActionArea, CardActions, CardContent, Typography, makeStyles, Theme } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
 import { useTranslation } from 'react-i18next';
 import { ButtonSecondary } from '../Button';
@@ -34,16 +34,20 @@ export const BlogArticleCard = ({ pictureUrl, title, description, link }: Props)
             // />
         // </Card>
         <Card className={classes.card}>
-            <CardMedia component="img" alt={title} image={pictureUrl} title={title} className={classes.cardImage} />
-            <CardContent className={classes.cardContent}>
-                <Typography gutterBottom variant="h5" component="h2">
-                    {title}
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                    {description}
-                </Typography>
-            </CardContent>
-            <CardActions>
+            <CardActionArea href={link} className={classes.actionArea}>
+                <CardMedia component="img" alt={title} image={pictureUrl} title={title} className={classes.cardImage} />
+                <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {title}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica. Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+                    </Typography>
+                </CardContent>
+                <CardActions className={classes.actions}>
                 <ButtonSecondary
                     href={link}
                     variant="contained"
@@ -52,47 +56,65 @@ export const BlogArticleCard = ({ pictureUrl, title, description, link }: Props)
                     disableElevation
                     innerText={t('blog-article-card.read-more')}
                 />
-            </CardActions>
+                </CardActions>
+            </CardActionArea>
+            {/* <CardActions className={classes.actions}>
+                   <ButtonSecondary
+                    href={link}
+                    variant="contained"
+                    startIcon={<SendIcon />}
+                    className={classes.readMoreButton}
+                    disableElevation
+                    innerText={t('blog-article-card.read-more')}
+                />
+            </CardActions> */}
         </Card>
     );
 };
 
 const useStyles = makeStyles((theme: Theme) => ({
     card: {
-        position: 'relative',
-        height: '400px',
-        // overflow: 'visible',
+        // position: 'relative',
+        height: '45vh',
         // backgroundColor: theme.palette.grey[300],
         // marginTop: '10 %',
         boxShadow: '1px 1px 4px 0 rgba(0, 0, 0, 0.15)',
-        borderRadius: '2%',
-        "&:hover": {
-            boxShadow: '1px 1px 7px 0 rgba(0, 0, 0, 0.4)',
-        },
+    },
+    actionArea: {
+        height: '100%',
     },
     cardImage: {
-        maxHeight: '200px',
+        height: '40%',
+        // maxHeight: '20vh',
         // padding: '0 16px',
-        position: 'relative',
+        // position: 'relative',
         // bottom: '20px',
     },
     cardContent: {
-        position: 'relative',
-        bottom: '20px',
-        marginTop: theme.spacing(1),
-        paddingBottom: 0,
-        height: '120px',
+        height: '45%',
+        // position: 'relative',
+        // bottom: '20px',
+        // marginTop: theme.spacing(1),
+        // paddingBottom: 0,
+        // height: '120px',
         wordBreak: 'break-word',
+        overflow: 'hidden',
     },
-    articleTitle: {
-        position: 'relative',
-        bottom: '10px',
+    actions: {
+        height: '15%',
     },
+    // articleTitle: {
+    //     position: 'relative',
+    //     bottom: '10px',
+    // },
     readMoreButton: {
         fontSize: '13px',
         position: 'absolute',
-        right: '16px',
-        bottom: '16px',
-        marginTop: '10px',
+        right: theme.spacing(2),
+        bottom: theme.spacing(2),
+        marginTop: theme.spacing(1),
     },
+    // actions: {
+    //     backgroundColor: 'blue',
+    // },
 }));
