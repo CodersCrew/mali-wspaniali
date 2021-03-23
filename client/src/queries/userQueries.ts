8c0a318265d8ae682ade6df807f04711a80c6920
Marek Kustosz <markust71@hotmail.com>
2021-03-23 17:30:05 +0100

    MALWSP-471: remove redundant console.log

--- client/src/queries/userQueries.ts
+++ client/src/queries/userQueries.ts
@@ -3,7 +3,5 @@ import { ReturnedStatus, UserInput } from '../graphql/types';
 import * as UserRepository from '../graphql/userRepository';
 
 export const createUser = (user: UserInput): Promise<FetchResult<ReturnedStatus>> => {
-    console.log(user);
-
     return UserRepository.createUser(user);
 };
