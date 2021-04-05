b64bfc618f203e7d11a55e6ee171a9d14c0d9d08
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2021-04-05 22:49:03 +0200

    adjust mappers, add tests

--- server/src/articles/domain/mappers/article_mapper.ts
+++ server/src/articles/domain/mappers/article_mapper.ts
@@ -1,11 +1,12 @@
-import { Article } from '../models/article_model';
+import { Article, ArticleInnerProps } from '../models/article_model';
 import { ArticleProps } from '../../../articles/domain/models/article_model';
 import { Category } from '../models/category';
 import { Url } from '../../../shared/domain/url';
-import { ReadingTime } from '../models/reading_time';
 import { Tags } from '../models/tags';
 import { TextLength } from '../../../shared/domain/text_length';
 import { RedactorMapper } from './redactor_mapper';
+import { ArticleInput } from '../../inputs/article_input';
+import { RedactorProps } from '../models/redactor';
 
 export class ArticleMapper {
   static toRaw(article: Article): ArticleProps {
@@ -16,7 +17,6 @@ export class ArticleMapper {
       category: props.category.value,
       pictureUrl: props.pictureUrl.value,
       videoUrl: props.videoUrl.value,
-      readingTime: props.readingTime.value,
       tags: props.tags.value,
       description: props.description.value,
       title: props.title.value,
@@ -26,11 +26,112 @@ export class ArticleMapper {
     return mappedProps;
   }
 
-  static toDomain(props: ArticleProps): Article {
+  static toPartialRaw(
+    updates: Partial<ArticleInnerProps>,
+  ): Partial<ArticleInput> {
+    const data: Partial<ArticleInput> = {};
+
+    if (updates.category) {
+      data.category = updates.category.value;
+    }
+
+    if (updates.pictureUrl) {
+      data.pictureUrl = updates.pictureUrl.value;
+    }
+
+    if (updates.videoUrl) {
+      data.videoUrl = updates.videoUrl.value;
+    }
+
+    if (updates.tags) {
+      data.tags = updates.tags.value;
+    }
+
+    if (updates.description) {
+      data.description = updates.description.value;
+    }
+
+    if (updates.title) {
+      data.title = updates.title.value;
+    }
+
+    if (updates.redactor) {
+      data.redactor = RedactorMapper.toPersistence(updates.redactor!);
+    }
+
+    return data;
+  }
+
+  static toPartialDomain(
+    updates: Partial<ArticleInput>,
+  ): Partial<ArticleInnerProps> {
+    const data: Partial<ArticleInnerProps> = {};
+
+    if (updates.category) {
+      const category = Category.create(updates.category).getValue();
+
+      data.category = category;
+    }
+
+    if (updates.pictureUrl) {
+      const pictureUrl = Url.create(updates.pictureUrl).getValue();
+
+      data.pictureUrl = pictureUrl;
+    }
+
+    if (updates.videoUrl) {
+      const videoUrl = Url.create(updates.videoUrl).getValue();
+
+      data.videoUrl = videoUrl;
+    }
+
+    if (updates.contentHTML) {
+      data.contentHTML = updates.contentHTML;
+    }
+
+    if (updates.tags) {
+      const tags = Tags.create(updates.tags).getValue();
+
+      data.tags = tags;
+    }
+
+    if (updates.description) {
+      const description = TextLength.create(
+        updates.description,
+        'description',
+        300,
+        30,
+      ).getValue();
+
+      data.description = description;
+    }
+
+    if (updates.title) {
+      const title = TextLength.create(
+        updates.title,
+        'title',
+        100,
+        10,
+      ).getValue();
+
+      data.title = title;
+    }
+
+    if (updates.redactor) {
+      const redactor = RedactorMapper.toDomain(
+        updates.redactor as RedactorProps,
+      ).getValue();
+
+      data.redactor = redactor;
+    }
+
+    return data;
+  }
+
+  static toDomain(props: ArticleInput): Article {
     const category = Category.create(props.category).getValue();
     const pictureUrl = Url.create(props.pictureUrl).getValue();
     const videoUrl = Url.create(props.videoUrl).getValue();
-    const readingTime = ReadingTime.create(props.readingTime).getValue();
     const tags = Tags.create(props.tags).getValue();
     const description = TextLength.create(
       props.description,
@@ -46,7 +147,6 @@ export class ArticleMapper {
       category,
       pictureUrl,
       videoUrl,
-      readingTime,
       tags,
       description,
       title,
