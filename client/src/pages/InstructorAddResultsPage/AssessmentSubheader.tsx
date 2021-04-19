b45253a8738dc7266213c0d230089267b8faba52
Tom-Bul <t.bulat99@gmail.com>
2021-04-19 13:41:45 +0200

    BUG-40-assessment-subheader-mobile
    Added assessment header for 'Instructor add results page' for mobile

--- client/src/pages/InstructorAddResultsPage/AssessmentSubheader.tsx
+++ client/src/pages/InstructorAddResultsPage/AssessmentSubheader.tsx
@@ -4,6 +4,7 @@ import { Assessment, AssessmentResult } from '../../graphql/types';
 import { StatusChip } from '../../components/StatusChip';
 import dayjs from '../../localizedMoment';
 import { countProgress } from '../InstructorResultCreatorPage/countProgress';
+import { useIsDevice } from '../../queries/useBreakpoints';
 
 interface Props {
     max: number;
@@ -16,16 +17,87 @@ export function AssessmentSubheader({ results, max, assessment }: Props) {
     const classes = useStyles();
     const currentMeasurement = getMostRecentMeasurement();
     const currentProgress = countProgressFromResults();
+    const device = useIsDevice();
 
     return (
-        <Grid container>
-            <Grid item xs={4}>
+        <div>
+            {!device.isSmallMobile ? (
+                <Grid container>
+                    <Grid item xs={4}>
+                        <Grid container direction="column" spacing={1}>
+                            <Grid item>
+                                <Typography variant="caption">{assessment.title}</Typography>
+                            </Grid>
+                            <Grid item>
+                                <Grid container spacing={1}>
+                                    <Grid item>
+                                        <Typography variant="h4">{currentMeasurement.label}</Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <StatusChip value={currentMeasurement.status} />
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                            <Grid item>
+                                <Grid container spacing={1}>
+                                    <Grid item>
+                                        <Typography variant="body2">
+                                            {currentMeasurement.startDate.split('-').join('.')}&nbsp;-&nbsp;
+                                            {currentMeasurement.endDate.split('-').join('.')}
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography variant="subtitle2">
+                                            ({dayjs(currentMeasurement.endDate).fromNow()})
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <Grid
+                            container
+                            direction="column"
+                            justify="flex-end"
+                            className={classes.progressBarContainer}
+                            spacing={1}
+                        >
+                            <Grid item>
+                                <Typography variant="body2">
+                                    {t(`add-results-page.${currentMeasurement.measurement}-assessment-progress`)}
+                                </Typography>
+                            </Grid>
+                            <Grid item>
+                                <Box display="flex" alignItems="flex-end">
+                                    <Box width="85%" mr={2}>
+                                        <LinearProgress
+                                            variant="determinate"
+                                            value={Math.floor((currentProgress * 100) / max)}
+                                            classes={{
+                                                root: classes.progressBar,
+                                                bar: classes.progressBarDark,
+                                                colorPrimary: classes.progressBarLight,
+                                            }}
+                                        />
+                                    </Box>
+                                    <Box>
+                                        <Typography variant="h4">
+                                            {Math.floor((currentProgress * 100) / max)}%
+                                        </Typography>
+                                    </Box>
+                                </Box>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            ) : (
                 <Grid container direction="column" spacing={1}>
                     <Grid item>
                         <Typography variant="caption">{assessment.title}</Typography>
                     </Grid>
                     <Grid item>
-                        <Grid container spacing={1}>
+                        <Grid container direction="row" spacing={1}>
                             <Grid item>
                                 <Typography variant="h4">{currentMeasurement.label}</Typography>
                             </Grid>
@@ -35,7 +107,7 @@ export function AssessmentSubheader({ results, max, assessment }: Props) {
                         </Grid>
                     </Grid>
                     <Grid item>
-                        <Grid container spacing={1}>
+                        <Grid container direction="row" spacing={1}>
                             <Grid item>
                                 <Typography variant="body2">
                                     {currentMeasurement.startDate.split('-').join('.')}&nbsp;-&nbsp;
@@ -49,42 +121,36 @@ export function AssessmentSubheader({ results, max, assessment }: Props) {
                             </Grid>
                         </Grid>
                     </Grid>
-                </Grid>
-            </Grid>
-            <Grid item xs={8}>
-                <Grid
-                    container
-                    direction="column"
-                    justify="flex-end"
-                    className={classes.progressBarContainer}
-                    spacing={1}
-                >
                     <Grid item>
-                        <Typography variant="body2">
-                            {t(`add-results-page.${currentMeasurement.measurement}-assessment-progress`)}
-                        </Typography>
-                    </Grid>
-                    <Grid item>
-                        <Box display="flex" alignItems="flex-end">
-                            <Box width="85%" mr={2}>
-                                <LinearProgress
-                                    variant="determinate"
-                                    value={Math.floor((currentProgress * 100) / max)}
-                                    classes={{
-                                        root: classes.progressBar,
-                                        bar: classes.progressBarDark,
-                                        colorPrimary: classes.progressBarLight,
-                                    }}
-                                />
-                            </Box>
-                            <Box>
-                                <Typography variant="h4">{Math.floor((currentProgress * 100) / max)}%</Typography>
-                            </Box>
+                        <Box className={classes.assessmentProgressText}>
+                            <Typography variant="body2">
+                                {t(`add-results-page.${currentMeasurement.measurement}-assessment-progress`)}
+                            </Typography>
                         </Box>
                     </Grid>
+                    <Grid item>
+                        <Grid container direction="row" spacing={1}>
+                            <Grid item xs={10}>
+                                <Box mt={1}>
+                                    <LinearProgress
+                                        variant="determinate"
+                                        value={Math.floor((currentProgress * 100) / max)}
+                                        classes={{
+                                            root: classes.progressBar,
+                                            bar: classes.progressBarDark,
+                                            colorPrimary: classes.progressBarLight,
+                                        }}
+                                    />
+                                </Box>
+                            </Grid>
+                            <Grid item xs={2}>
+                                <Typography variant="h4">{Math.floor((currentProgress * 100) / max)}%</Typography>
+                            </Grid>
+                        </Grid>
+                    </Grid>
                 </Grid>
-            </Grid>
-        </Grid>
+            )}
+        </div>
     );
 
     function getMostRecentMeasurement() {
@@ -127,6 +193,9 @@ const useStyles = makeStyles((theme: Theme) =>
         },
         progressBarLight: {
             background: theme.palette.success.light,
+        },
+        assessmentProgressText: {
+            width: theme.spacing(40),
         },
     }),
 );
