c04b5f58e841560f3acec731c415b8219a2b16b3
Marek Kustosz <markust71@hotmail.com>
2021-04-19 13:41:50 +0200

    MALWSP: remove unnecessary comments

--- client/src/pages/AdminCodesPage/KeyCodes/useGenerateExcel.tsx
+++ client/src/pages/AdminCodesPage/KeyCodes/useGenerateExcel.tsx
@@ -12,7 +12,6 @@ export function useGenerateExcel(onCreate: (filename: string) => void) {
 
             if (keyCodes.length > 0) {
                 const filename = `mw-keycodes-${series}.xlsx`;
-                // const workbook = getKeyCodesWorkbook(keyCodes);
                 const workbook = getKeyCodesWorkbook(
                     keyCodes.map((code) => {
                         return { ...code, keyCode: `${code.target === 'parent' ? 'P' : 'I'}.${code.keyCode}` };
