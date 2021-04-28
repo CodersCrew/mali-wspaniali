10b8deef725596cbe60e59e7e8abd63707b3e273
Tom-Bul <t.bulat99@gmail.com>
2021-04-29 00:07:32 +0200

    added pagination to admin and style fixes

--- client/src/pages/AdminKindergartensPage/KindergartensTable/KindergartensTable.tsx
+++ client/src/pages/AdminKindergartensPage/KindergartensTable/KindergartensTable.tsx
@@ -1,5 +1,15 @@
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Box } from '@material-ui/core';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Box,
+    TablePagination,
+} from '@material-ui/core';
 import { KindergartensTableRow } from './KindergartensTableRow';
 import { Kindergarten } from '../../../graphql/types';
 
@@ -10,6 +20,17 @@ interface Props {
 
 export const KindergartensTable = ({ kindergartens, onEditClick }: Props) => {
     const { t } = useTranslation();
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
+
+    const handleChangePage = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
 
     return (
         <TableContainer component={Box}>
@@ -23,7 +44,10 @@ export const KindergartensTable = ({ kindergartens, onEditClick }: Props) => {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {kindergartens.map((kindergarten) => (
+                    {(rowsPerPage > 0
+                        ? kindergartens.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                        : kindergartens
+                    ).map((kindergarten) => (
                         <KindergartensTableRow
                             key={kindergarten._id}
                             kindergarten={kindergarten}
@@ -32,6 +56,16 @@ export const KindergartensTable = ({ kindergartens, onEditClick }: Props) => {
                     ))}
                 </TableBody>
             </Table>
+            <TablePagination
+                rowsPerPageOptions={[5, 10, 25]}
+                component="div"
+                labelRowsPerPage={t('test-results.rows-number')}
+                count={kindergartens.length}
+                page={page}
+                onChangePage={handleChangePage}
+                rowsPerPage={rowsPerPage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+            />
         </TableContainer>
     );
 };
