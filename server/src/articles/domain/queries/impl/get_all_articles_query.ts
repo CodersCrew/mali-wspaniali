ebbe8d16c1050ccdfd0800a712cce9f02221028d
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2021-05-30 00:38:45 +0200

    update article model with class transformer, class validator, add published, deleted fields

--- server/src/articles/domain/queries/impl/get_all_articles_query.ts
+++ server/src/articles/domain/queries/impl/get_all_articles_query.ts
@@ -1,7 +1,10 @@
+import { LoggedUser } from '../../../../users/params/current_user_param';
+
 export class GetAllArticlesQuery {
   constructor(
     public readonly page: number,
     public readonly perPage: number,
+    public readonly user: LoggedUser,
     public readonly category?: string,
   ) {}
 }
