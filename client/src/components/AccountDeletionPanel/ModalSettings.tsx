dd14eb35303c325f087068eab21d0acea08c9ff1
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2021-06-02 01:10:58 +0200

    inline dialog component, inline types, revert BasicDialog prop

--- client/src/components/AccountDeletionPanel/ModalSettings.tsx
+++ client/src/components/AccountDeletionPanel/ModalSettings.tsx
@@ -7,7 +7,7 @@ import { Select } from '../ChildForm/Select';
 import { Input } from '../ChildForm/Input';
 import { BasicModal } from '../Modal/BasicModal';
 
-import { openDialog, ActionDialog } from '../../utils/openDialog';
+import { openDialog } from '../../utils/openDialog';
 import { useParentsSelectOptions } from './useParentsSelectOption';
 import { Me } from '../../graphql/types';
 
@@ -31,87 +31,86 @@ const normalizeParent = (parent: FormValues) => {
     };
 };
 
-const SettingsMessageModal = ({ onClose, user, makeDecision }: { user: Me } & ActionDialog<{ parent: FormValues }>) => {
-    const { t } = useTranslation();
-    const classes = useStyles();
-    const { getOptions } = useParentsSelectOptions();
-
-    const initialValues: FormValues = {
-        email: user.mail,
-        messageTopic: 'deleteAccount',
-        message: '',
-    };
-
-    const formik = useFormik({
-        initialValues,
-        validationSchema,
-        onSubmit: (values) => {
-            makeDecision({ accepted: true, parent: normalizeParent(values) });
-        },
-    });
-
-    return (
-        <BasicModal
-            isOpen={true}
-            actionName={t('settings-modal.button')}
-            onAction={formik.handleSubmit}
-            onClose={onClose}
-            isActionButtonVisible
-        >
-            <form onSubmit={formik.handleSubmit}>
-                <Box>
-                    <Typography variant={'h4'} className={classes.header}>
-                        {t('settings-modal.modal-header')}
-                    </Typography>
-                    <Box mt={4} mb={2.5}>
-                        <Typography variant={'body1'}>{t('settings-modal.modal-first-description')}</Typography>
-                        <Typography variant={'body1'}>{t('settings-modal.modal-second-description')}</Typography>
-                    </Box>
-                    <Grid direction="column" container>
-                        <Grid item>
-                            <Input
-                                multiline={true}
-                                name="email"
-                                label={t('settings-modal.text-field-email-label')}
-                                value={user.mail}
-                                error={formik.errors.email}
-                                touched={formik.touched.email}
-                                onChange={(name: string, value: string) => formik.setFieldValue(name, value)}
-                                disabled={true}
-                            />
-                        </Grid>
-                        <Grid item className={classes.inputBox}>
-                            <Select
-                                label={t('settings-modal.select-options.select-label')}
-                                value={formik.values.messageTopic}
-                                options={getOptions('settings-message')}
-                                name="messageTopic"
-                                error={formik.errors.messageTopic}
-                                touched={formik.touched.messageTopic}
-                                onChange={(name: string, value: string) => formik.setFieldValue(name, value)}
-                            />
-                        </Grid>
-                        <Grid item>
-                            <Input
-                                name="message"
-                                label={t('settings-modal.text-field-title-label')}
-                                value={formik.values.message}
-                                error={formik.errors.message}
-                                touched={formik.touched.message}
-                                onChange={(name: string, value: string) => formik.setFieldValue(name, value)}
-                                multiline
-                                rows={10}
-                            />
-                        </Grid>
-                    </Grid>
-                </Box>
-            </form>
-        </BasicModal>
-    );
-};
-
 export const openSettingsModal = (props: { user: Me }) => {
-    return openDialog<{ user: Me }>(SettingsMessageModal, props);
+    return openDialog<{ user: Me }>(function SettingsMessageModal({ onClose, user, makeDecision }) {
+        const { t } = useTranslation();
+        const classes = useStyles();
+        const { getOptions } = useParentsSelectOptions();
+
+        const initialValues: FormValues = {
+            email: user.mail,
+            messageTopic: 'deleteAccount',
+            message: '',
+        };
+
+        const formik = useFormik({
+            initialValues,
+            validationSchema,
+            onSubmit: (values) => {
+                makeDecision({ accepted: true, parent: normalizeParent(values) });
+            },
+        });
+
+        return (
+            <BasicModal
+                isOpen={true}
+                actionName={t('settings-modal.button')}
+                onAction={formik.handleSubmit}
+                onClose={onClose}
+                isActionButtonVisible
+                isCancelButtonVisible
+            >
+                <form onSubmit={formik.handleSubmit}>
+                    <Box>
+                        <Typography variant={'h4'} className={classes.header}>
+                            {t('settings-modal.modal-header')}
+                        </Typography>
+                        <Box mt={4} mb={2.5}>
+                            <Typography variant={'body1'}>{t('settings-modal.modal-first-description')}</Typography>
+                            <Typography variant={'body1'}>{t('settings-modal.modal-second-description')}</Typography>
+                        </Box>
+                        <Grid direction="column" container>
+                            <Grid item>
+                                <Input
+                                    multiline={true}
+                                    name="email"
+                                    label={t('settings-modal.text-field-email-label')}
+                                    value={user.mail}
+                                    error={formik.errors.email}
+                                    touched={formik.touched.email}
+                                    onChange={(name: string, value: string) => formik.setFieldValue(name, value)}
+                                    disabled={true}
+                                />
+                            </Grid>
+                            <Grid item className={classes.inputBox}>
+                                <Select
+                                    label={t('settings-modal.select-options.select-label')}
+                                    value={formik.values.messageTopic}
+                                    options={getOptions('settings-message')}
+                                    name="messageTopic"
+                                    error={formik.errors.messageTopic}
+                                    touched={formik.touched.messageTopic}
+                                    onChange={(name: string, value: string) => formik.setFieldValue(name, value)}
+                                />
+                            </Grid>
+                            <Grid item>
+                                <Input
+                                    name="message"
+                                    label={t('settings-modal.text-field-title-label')}
+                                    value={formik.values.message}
+                                    error={formik.errors.message}
+                                    touched={formik.touched.message}
+                                    onChange={(name: string, value: string) => formik.setFieldValue(name, value)}
+                                    multiline
+                                    rows={10}
+                                />
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </form>
+            </BasicModal>
+        );
+    }, props);
 };
 
 const useStyles = makeStyles((theme) => ({
