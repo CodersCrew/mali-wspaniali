b470f64d659f9a877d7cfc9fbdcc986b67807b0c
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2021-07-04 18:29:49 +0200

    kindergarten count result stats

---
+++ server/src/shared/utils/count_results.ts
@@ -0,0 +1,27 @@
+import { ChildAssessmentResult } from '../../users/domain/models/child_assessment_result_model';
+
+export function countResults(
+  results: ChildAssessmentResult[],
+  measurement: string,
+) {
+  let measurements = 0;
+
+  results.forEach(result => {
+    !(result instanceof ChildAssessmentResult) && console.log(result);
+    const props = result.getProps();
+
+    if (measurement === 'first') {
+      if (props.firstMeasurementJumpDate) ++measurements;
+      if (props.firstMeasurementRunDate) ++measurements;
+      if (props.firstMeasurementPendelumRunDate) ++measurements;
+      if (props.firstMeasurementThrowDate) ++measurements;
+    } else {
+      if (props.lastMeasurementJumpDate) ++measurements;
+      if (props.lastMeasurementRunDate) ++measurements;
+      if (props.lastMeasurementPendelumRunDate) ++measurements;
+      if (props.lastMeasurementThrowDate) ++measurements;
+    }
+  });
+
+  return measurements;
+}
