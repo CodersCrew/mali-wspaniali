be2ddc14ccc7a4b0ce4f404557f4562a9a8df487
Piotr Pietruszka <piotr.pietruszka@o2.pl>
2021-08-06 23:54:11 +0200

    refetch articles after creation

--- client/src/operations/mutations/Articles/createArticle.ts
+++ client/src/operations/mutations/Articles/createArticle.ts
@@ -2,6 +2,7 @@ import { gql, useMutation } from '@apollo/client';
 import { useTranslation } from 'react-i18next';
 import { openSnackbar } from '../../../components/Snackbar/openSnackbar';
 import { Article } from '../../../graphql/types';
+import { useArticles } from '../../queries/Articles/getArticles';
 
 export interface CreatedArticleInput {
     title: string;
@@ -29,6 +30,7 @@ const CREATE_ARTICLE = gql`
 
 export function useCreateArticle() {
     const [mutate] = useMutation<CreateArticleResponse>(CREATE_ARTICLE);
+    const { refetch } = useArticles(1);
     const { t } = useTranslation();
 
     return {
@@ -48,8 +50,9 @@ export function useCreateArticle() {
                         },
                     },
                 },
-            }).then((result) => {
+            }).then(async (result) => {
                 openSnackbar({ text: t('add-article.events.created') });
+                await refetch();
 
                 return result.data?.createArticle?._id;
             });
