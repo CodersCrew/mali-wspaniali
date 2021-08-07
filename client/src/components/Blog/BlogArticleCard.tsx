00aef420b76c2b662b51d0ff1c1fd8affbaa2ae1
Piotr Pietruszka <piotr.pietruszka@o2.pl>
2021-08-07 21:30:36 +0200

    hide or publish articles

--- client/src/components/Blog/BlogArticleCard.tsx
+++ client/src/components/Blog/BlogArticleCard.tsx
@@ -59,7 +59,7 @@ export const BlogArticleCard = (props: BlogArticleCardProps) => {
                         </IconButton>
                     </Tooltip>
                     <Tooltip title={getPublishUnpublishTitle()}>
-                        <IconButton>
+                        <IconButton onClick={onPublishOrHideArticleClick}>
                             <Public color={props.article.isPublished ? 'secondary' : 'inherit'} />
                         </IconButton>
                     </Tooltip>
@@ -87,6 +87,22 @@ export const BlogArticleCard = (props: BlogArticleCardProps) => {
             if (response.close || !response.decision?.accepted) return;
 
             updateArticle({ ...props.article, isDeleted: true }).then(refetch);
+        });
+    }
+
+    function onPublishOrHideArticleClick() {
+        openQuestionDialog({
+            title: t(
+                props.article.isPublished
+                    ? 'admin-article-list.card.hide-article-dialog.title'
+                    : 'admin-article-list.card.publish-article-dialog.title',
+                { title: props.article.title },
+            ),
+            primaryButtonLabel: t(props.article.isPublished ? 'question-dialog.hide' : 'question-dialog.publish'),
+        }).then((response) => {
+            if (response.close || !response.decision?.accepted) return;
+
+            updateArticle({ ...props.article, isPublished: !props.article.isPublished }).then(refetch);
         });
     }
 };
