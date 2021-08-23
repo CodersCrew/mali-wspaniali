44ebc027a21068c06aaf3e5692dc3e41c73ec507
Piotr Pietruszka <piotr.pietruszka@o2.pl>
2021-08-23 20:14:44 +0200

    add tests

---
+++ server/src/assessment/domain/commands/handlers/__tests__/update_assessment_handler.test.ts
@@ -0,0 +1,73 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import * as dbHandler from '@app/db_handler';
+
+import { Assessment } from '../../../models/assessment_model';
+import { AssessmentModule } from '../../../../assessment_module';
+import { createAssessment, getAssessment } from '@app/test/helpers/app_mock';
+import { UpdateAssessmentHandler } from '../update_assessment_handler';
+import { UpdateAssessmentCommand } from '../../impl';
+import { UpdatedAssessmentInput } from '../../../../inputs/assessment_input';
+
+describe('CreateAssessmentHandler', () => {
+  let assessment: Assessment;
+  let app: TestingModule;
+
+  const validAssessmentOptions = {
+    title: 'my-title',
+    firstMeasurementStatus: 'active',
+    lastMeasurementStatus: 'active',
+    firstMeasurementStartDate: new Date(2020, 10, 16),
+    firstMeasurementEndDate: new Date(2020, 10, 20),
+    lastMeasurementStartDate: new Date(2020, 10, 16),
+    lastMeasurementEndDate: new Date(2020, 10, 20),
+    kindergartenIds: ['5f88ea2c6d80f367f66a1692'],
+  };
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  beforeEach(async () => {
+    app = await setup();
+
+    await dbHandler.clearDatabase();
+
+    assessment = await createAssessment(validAssessmentOptions);
+  });
+
+  it('updated assessment', async () => {
+    expect(assessment.title).toBe('my-title');
+    expect(assessment.groups).toEqual([]);
+
+    await updateAssessmentWith(assessment.id, { title: 'my-new-title' });
+
+    expect((await getAssessment(assessment.id)).title).toBe('my-new-title');
+
+    await updateAssessmentWith(assessment.id, {
+      groups: [{ kindergartenId: 'some-kindergarten', group: 'my-name' }],
+    });
+
+    expect((await getAssessment(assessment.id)).groups).toEqual([
+      { kindergartenId: 'some-kindergarten', group: 'my-name' },
+    ]);
+  });
+
+  function updateAssessmentWith(
+    id: string,
+    assessment: UpdatedAssessmentInput,
+  ) {
+    return app.resolve(UpdateAssessmentHandler).then(handler => {
+      return handler.execute(new UpdateAssessmentCommand(id, assessment));
+    });
+  }
+
+  async function setup() {
+    const module = await Test.createTestingModule({
+      imports: [dbHandler.rootMongooseTestModule(), AssessmentModule],
+    }).compile();
+
+    await module.init();
+
+    return module;
+  }
+});
