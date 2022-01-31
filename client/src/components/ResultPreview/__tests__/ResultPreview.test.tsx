5870f5e53031dd1ab134432400581a037e0c4484
Piotr Pietruszka <piotr.pietruszka@o2.pl>
2022-01-31 22:19:44 +0100

    tests: adjust test setup

--- client/src/components/ResultPreview/__tests__/ResultPreview.test.tsx
+++ client/src/components/ResultPreview/__tests__/ResultPreview.test.tsx
@@ -1,19 +1,36 @@
-import { ResultPreview } from '../ResultPreview';
-import { renderWithMock } from '@app/utils/testing/renderWithMockedProvider';
+import { screen } from '@testing-library/dom';
+
 import { translationOf } from '@app/utils/testing/isTranslationOf';
+import { renderWithMock } from '@app/utils/testing/renderWithMockedProvider';
+
+import { ResultPreview } from '../ResultPreview';
 
 describe('ResultPreview', () => {
     describe('when result exists', () => {
-        it('renders preview with test stages', async () => {
+        it('renders preview with test stages and summary', async () => {
             await renderWithMock(<ResultPreview resultId="my-result-id" />);
 
-            expect(document.querySelector('section:nth-of-type(1) h4')?.textContent).toBe(
-                translationOf('child-profile.initial-test'),
-            );
+            expect(screen.queryByText(translationOf('child-profile.initial-test'))).toBeDefined();
+            expect(screen.queryByText(translationOf('child-profile.final-test'))).toBeDefined();
+            expect(screen.queryByText(translationOf('child-profile.test-result'))).toBeDefined();
+        });
 
-            expect(document.querySelector('section:nth-of-type(2) h4')?.textContent).toBe(
-                translationOf('child-profile.final-test'),
-            );
+        describe('and assessment is partly done', () => {
+            it('renders preview with only the first measurement', async () => {
+                await renderWithMock(<ResultPreview resultId="only-first-done" />);
+
+                expect(screen.queryByText(translationOf('child-profile.initial-test'))).toBeDefined();
+                expect(screen.queryByText(translationOf('child-profile.final-test'))).toBeNull();
+                expect(screen.queryByText(translationOf('child-profile.test-result'))).toBeNull();
+            });
+
+            it('renders preview with only the last measurement', async () => {
+                await renderWithMock(<ResultPreview resultId="only-last-done" />);
+
+                expect(screen.queryByText(translationOf('child-profile.initial-test'))).toBeNull();
+                expect(screen.queryByText(translationOf('child-profile.final-test'))).toBeDefined();
+                expect(screen.queryByText(translationOf('child-profile.test-result'))).toBeNull();
+            });
         });
     });
 });
--- client/src/mocks/handlers.ts
+++ client/src/mocks/handlers.ts
@@ -1,16 +1,19 @@
-import { graphql } from 'msw';
+import { graphql, GraphQLRequest } from 'msw';
 import { getAssessmentMock } from '@app/utils/testing/getAssessmentMock';
 import { getChildMock } from '@app/utils/testing/getChildMock';
 import { getCurrentParamMock } from '@app/utils/testing/getCurrentParamMock';
 import { getResultMock } from '@app/utils/testing/getResultMock';
+import { Assessment } from '@app/graphql/types';
 
 export const handlers = [
-    graphql.query('Result', (req, res, ctx) => {
+    graphql.query('Result', (req: GraphQLRequest<{ id: string }>, res, ctx) => {
+        const reqVariables = req.body!.variables as { id: string };
+
         return res(
             ctx.data({
                 result: {
                     ...getResultMock(),
-                    assessment: getAssessmentMock(),
+                    assessment: getAssessmentMock(getAssessmentChange(reqVariables.id)),
                     currentParams: getCurrentParamMock(),
                     child: getChildMock(),
                 },
@@ -18,3 +21,15 @@ export const handlers = [
         );
     }),
 ];
+
+function getAssessmentChange(id: string): Partial<Assessment> {
+    if (id === 'only-first-done') {
+        return { firstMeasurementStatus: 'done', lastMeasurementStatus: 'active' };
+    }
+
+    if (id === 'only-last-done') {
+        return { firstMeasurementStatus: 'active', lastMeasurementStatus: 'done' };
+    }
+
+    return { firstMeasurementStatus: 'done', lastMeasurementStatus: 'done' };
+}
