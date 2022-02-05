bf8661ecec45de0724b3f70125d02dbe80dbef46
Piotr Pietruszka <piotr.pietruszka@o2.pl>
2022-02-05 18:01:12 +0100

    test(setup): extract common setup into global module

--- server/setupTests.ts
+++ server/setupTests.ts
@@ -1,3 +1,16 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import * as dbHandler from '@app/db_handler';
+import { CqrsModule } from '@nestjs/cqrs';
+import { UsersModule } from './src/users/users_module';
+import { AgreementsModule } from './src/agreements/agreements_module';
+import { KeyCodesModule } from './src/key_codes/key_codes_module';
+import { KindergartenModule } from './src/kindergartens/kindergarten_module';
+import { NewslettersModule } from './src/newsletters/newsletters_module';
+import { CreateKeyCodeHandler } from './src/key_codes/domain/commands/handlers/create_key_code_handler';
+import { CreateUserHandler } from './src/users/domain/commands/handlers/create_user_handler';
+import { NotificationsModule } from './src/notifications/notifications.module';
+import { ArticlesModule } from './src/articles/articles_module';
+
 expect.extend({
   async toHaveValidationError(received, error): Promise<any> {
     try {
@@ -45,3 +58,38 @@ expect.extend({
     }
   },
 });
+
+async function setup() {
+  const module = await Test.createTestingModule({
+    imports: [
+      dbHandler.rootMongooseTestModule(),
+      AgreementsModule,
+      ArticlesModule,
+      CqrsModule,
+      KeyCodesModule,
+      KindergartenModule,
+      NewslettersModule,
+      NotificationsModule,
+      UsersModule,
+    ],
+    providers: [CreateKeyCodeHandler, CreateUserHandler],
+  }).compile();
+
+  await module.init();
+
+  return module;
+}
+
+let app: TestingModule;
+
+beforeAll(async () => {
+  app = await setup();
+});
+
+afterAll(async () => {
+  await app.close();
+});
+
+export function getApp() {
+  return app;
+}
