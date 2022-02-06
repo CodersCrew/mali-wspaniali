b0b79398c43f975632c4dc27c400ccebe1619a7a
Piotr Pietruszka <piotr.pietruszka@o2.pl>
2022-02-06 02:53:58 +0100

    test(Create Newsletter handler): add test for private message and for the message for all parents

--- server/src/newsletters/domain/commands/handlers/__tests__/create_newsletter_handler.test.ts
+++ server/src/newsletters/domain/commands/handlers/__tests__/create_newsletter_handler.test.ts
@@ -3,51 +3,80 @@ import waitForExpect from 'wait-for-expect';
 import { CreateNewsletterHandler } from '../create_newsletter_handler';
 import { CreateNewsletterCommand } from '../../impl/create_newsletter_command';
 import { NewsletterInput } from '../../../../inputs/newsletter_input';
-
-import { GetAllNewsletterHandler } from '../../../queries/handlars/get_all_newsletters_handler';
 import { SendMail } from '../../../../../shared/services/send_mail/send_mail';
 import { getApp } from '../../../../../../setupTests';
-import { awaitForResponse } from '../../../../../test/helpers/app_mock';
+import { createParent } from '@app/test/helpers/app_mock';
+import { getNewsletters } from '../../../../../test/helpers/app_mock';
 
 describe('CreateNewsletterHandler', () => {
   let sendMail: SendMail;
 
-  describe('when executed', () => {
-    beforeEach(async () => {
-      sendMail = getApp().get(SendMail);
+  beforeEach(async () => {
+    sendMail = getApp().get(SendMail);
+  });
+
+  it('sends a message to admin', async () => {
+    const newsletterOptions = {
+      message: 'My message to admin',
+      recipients: ['fundacja@mali-wspaniali.pl'],
+      title: 'My title',
+      type: 'PRIVATE',
+    };
+
+    const sendSpy = jest.spyOn(sendMail, 'send');
+    sendSpy.mockImplementation(() => null);
+    await createNewsletterWith(newsletterOptions);
+
+    await waitForExpect(async () => {
+      const newsletters = await getNewsletters();
+
+      expect(newsletters[0].title).toBe('My title');
+      expect(newsletters[0].message).toBe('My message to admin');
+      expect(newsletters[0].recipients).toEqual(['fundacja@mali-wspaniali.pl']);
+
+      expect(sendSpy).toBeCalledWith(
+        expect.objectContaining({
+          bcc: ['fundacja@mali-wspaniali.pl'],
+          from: 'my-admin@admin.com',
+          subject: 'My title',
+        }),
+      );
+
+      expect(sendSpy.mock.calls[0][0].html).toMatch('My title');
+      expect(sendSpy.mock.calls[0][0].html).toMatch('My message to admin');
     });
+  });
 
-    it('sends a newsletter', async () => {
-      const newsletterOptions = {
-        message: 'My message',
-        recipients: [],
-        title: 'My title',
-        type: 'ALL KINDERGARTENS',
-      };
+  it('sends a newsletter for all parents', async () => {
+    await createParent({ mail: 'my-mail@mail.com' });
+    await createParent({ mail: 'my-second-mail@mail.com' });
+    const newsletterOptions = {
+      message: 'My message',
+      recipients: [],
+      title: 'My title',
+      type: 'ALL KINDERGARTENS',
+    };
 
-      const sendSpy = jest.spyOn(sendMail, 'send');
-      sendSpy.mockImplementation(() => null);
-      await createNewsletterWith(newsletterOptions);
+    const sendSpy = jest.spyOn(sendMail, 'send');
+    sendSpy.mockImplementation(() => null);
+    await createNewsletterWith(newsletterOptions);
 
-      await waitForExpect(async () => {
-        const newsletters = await getNewsletterWith();
+    await waitForExpect(async () => {
+      const newsletters = await getNewsletters();
 
-        expect(newsletters[0].title).toBe('My title');
-        expect(newsletters[0].message).toBe('My message');
-        expect(newsletters[0].recipients).toEqual([]);
+      expect(newsletters[0].title).toBe('My title');
+      expect(newsletters[0].message).toBe('My message');
+      expect(newsletters[0].recipients).toEqual([]);
 
-        await awaitForResponse();
-
-        expect(sendSpy).toBeCalledWith(
-          expect.objectContaining({
-            bcc: [],
-            from: 'my-admin@admin.com',
-            subject: 'My title',
-          }),
-        );
-        expect(sendSpy.mock.calls[0][0].html).toMatch('My title');
-        expect(sendSpy.mock.calls[0][0].html).toMatch('My message');
-      });
+      expect(sendSpy).toBeCalledWith(
+        expect.objectContaining({
+          bcc: ['my-mail@mail.com', 'my-second-mail@mail.com'],
+          from: 'my-admin@admin.com',
+          subject: 'My title',
+        }),
+      );
+      expect(sendSpy.mock.calls[0][0].html).toMatch('My title');
+      expect(sendSpy.mock.calls[0][0].html).toMatch('My message');
     });
   });
 
@@ -62,11 +91,5 @@ describe('CreateNewsletterHandler', () => {
     await getApp()
       .get(CreateNewsletterHandler)
       .execute(new CreateNewsletterCommand({ ...defaultOptions, ...options }));
-  }
-
-  async function getNewsletterWith(options: Partial<NewsletterInput> = {}) {
-    return await getApp()
-      .get(GetAllNewsletterHandler)
-      .execute();
   }
 });
